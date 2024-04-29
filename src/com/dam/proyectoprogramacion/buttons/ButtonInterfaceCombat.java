a528fea03c298bba4c781eabd9e1af9b078879fa
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-04-29 20:53:04 +0200

    javadoc de los metodos que crean los botones de atacar y cambiar de pokemon

--- src/com/dam/proyectoprogramacion/buttons/ButtonInterfaceCombat.java
+++ src/com/dam/proyectoprogramacion/buttons/ButtonInterfaceCombat.java
@@ -4,11 +4,26 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
 
+/**
+ * Clase con los botones de la interfaz de combate pokemon
+ * Esto será un panek con los botones de atacar y cambiar de pokemon
+ * @author cristian
+ * @version v1.0
+ */
 public class ButtonInterfaceCombat extends JPanel {
 
+    /**
+     * atributos privados de tipo JButton
+     * atacar y escoger el ataque del pokemon
+     * cambiar y poder elegir que pokemon quieres usar
+     */
     private JButton attackButton;
     private JButton changePokemonButton;
 
+    /**
+     * metodo para crear el boton de atacar en el combate pokemon
+     * @return el boton creado
+     */
     public JButton makeAttackButton(){
         attackButton = new JButton("Atacar");
         setBackground(Color.RED);
@@ -17,6 +32,11 @@ public class ButtonInterfaceCombat extends JPanel {
         setBorder(borderboton);
         return attackButton;
     }
+
+    /**
+     * metodo para crear el boton de cambiar de pokemon
+     * @return el boton creado
+     */
     public JButton makeChangePokemonButton(){
         changePokemonButton = new JButton("Pokemon");
         setBackground(Color.GREEN);
