e6574fde0214ded1d638226728515102f519b41d
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-04-29 11:46:09 +0200

    Javadoc del panel con la imagen de fondo del menu principal

--- src/com/dam/proyectoprogramacion/panels/BackgroundMainMenuPanel.java
+++ src/com/dam/proyectoprogramacion/panels/BackgroundMainMenuPanel.java
@@ -3,14 +3,30 @@ package com.dam.proyectoprogramacion.panels;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Clase con  imagen de fondo del menú principal
+ * @author cristian
+ * @version v1.0
+ */
 public class BackgroundMainMenuPanel extends JPanel {
 
+    /**
+     * atributo privado de tipo imagen que contendrá la ruta de la imagen del menu principal
+     */
     private Image backgroundImage;
 
+    /**
+     * contructor de la clase
+     * @param backgroundImage el atributo de tipo imagen
+     */
     public BackgroundMainMenuPanel(Image backgroundImage) {
         this.backgroundImage = backgroundImage;
     }
 
+    /**
+     * metodo para poder pintar la imagen en el fondo del panel
+     * @param g the <code>Graphics</code> object to protect el objeto que va a pintar la imagen
+     */
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -20,6 +36,10 @@ public class BackgroundMainMenuPanel extends JPanel {
         }
     }
 
+    /**
+     * setter para establecer la imagen de fondo
+      * @param backgroundImage el atributo de tipo imagen
+     */
     public void setBackgroundImage(Image backgroundImage) {
         this.backgroundImage = backgroundImage;
         repaint();
