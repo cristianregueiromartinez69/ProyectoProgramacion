27338f41fdc835969a1cd26523e606941616b159
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-04-29 19:57:45 +0200

    creando los botones de los iconos del jugador 1

--- src/com/dam/proyectoprogramacion/buttons/ButtonInterfaceCreateAccount.java
+++ src/com/dam/proyectoprogramacion/buttons/ButtonInterfaceCreateAccount.java
@@ -32,7 +32,7 @@ public class ButtonInterfaceCreateAccount extends JPanel {
     private JButton readyPlayer2Button;
     private JButton backToMenuButton;
 
-    public JButton makeIcon1Button(){
+    public JButton makeIcon1Player1Button(){
         ImageIcon icon1 = new ImageIcon("imagenes/iconosJugador1.png");
         icon1Player1Button = new CreateButtons("Icono 1", icon1);
         setBackground(Color.YELLOW.darker());
@@ -40,6 +40,87 @@ public class ButtonInterfaceCreateAccount extends JPanel {
         Border borderboton = BorderFactory.createLineBorder(Color.GREEN.darker().darker(), 2);
         setBorder(borderboton);
         return icon1Player1Button;
+    }
+    public JButton makeIcon2Player1Button(){
+        ImageIcon icon2 = new ImageIcon("imagenes/iconosJugador1.png");
+        icon2Player1Button = new CreateButtons("Icono 2", icon2);
+        setBackground(Color.YELLOW.darker());
+        setForeground(Color.BLACK);
+        Border borderboton = BorderFactory.createLineBorder(Color.GREEN.darker().darker(), 2);
+        setBorder(borderboton);
+        return icon2Player1Button;
+    }
+    public JButton makeIcon3Player1Button(){
+        ImageIcon icon3 = new ImageIcon("imagenes/iconosJugador1.png");
+        icon3Player1Button = new CreateButtons("Icono 3", icon3);
+        setBackground(Color.YELLOW.darker());
+        setForeground(Color.BLACK);
+        Border borderboton = BorderFactory.createLineBorder(Color.GREEN.darker().darker(), 2);
+        setBorder(borderboton);
+        return icon3Player1Button;
+    }
+    public JButton makeIcon4Player1Button(){
+        ImageIcon icon4 = new ImageIcon("imagenes/iconosJugador1.png");
+        icon4Player1Button = new CreateButtons("Icono 4", icon4);
+        setBackground(Color.YELLOW.darker());
+        setForeground(Color.BLACK);
+        Border borderboton = BorderFactory.createLineBorder(Color.GREEN.darker().darker(), 2);
+        setBorder(borderboton);
+        return icon4Player1Button;
+    }
+    public JButton makeIcon5Player1Button(){
+        ImageIcon icon5 = new ImageIcon("imagenes/iconosJugador1.png");
+        icon5Player1Button = new CreateButtons("Icono 5", icon5);
+        setBackground(Color.YELLOW.darker());
+        setForeground(Color.BLACK);
+        Border borderboton = BorderFactory.createLineBorder(Color.GREEN.darker().darker(), 2);
+        setBorder(borderboton);
+        return icon5Player1Button;
+    }
+    public JButton makeIcon6Player1Button(){
+        ImageIcon icon6 = new ImageIcon("imagenes/iconosJugador1.png");
+        icon6Player1Button = new CreateButtons("Icono 6", icon6);
+        setBackground(Color.YELLOW.darker());
+        setForeground(Color.BLACK);
+        Border borderboton = BorderFactory.createLineBorder(Color.GREEN.darker().darker(), 2);
+        setBorder(borderboton);
+        return icon6Player1Button;
+    }
+    public JButton makeIcon7Player1Button(){
+        ImageIcon icon7 = new ImageIcon("imagenes/iconosJugador1.png");
+        icon7Player1Button = new CreateButtons("Icono 7", icon7);
+        setBackground(Color.YELLOW.darker());
+        setForeground(Color.BLACK);
+        Border borderboton = BorderFactory.createLineBorder(Color.GREEN.darker().darker(), 2);
+        setBorder(borderboton);
+        return icon7Player1Button;
+    }
+    public JButton makeIcon8Player1Button(){
+        ImageIcon icon8 = new ImageIcon("imagenes/iconosJugador1.png");
+        icon8Player1Button = new CreateButtons("Icono 8", icon8);
+        setBackground(Color.YELLOW.darker());
+        setForeground(Color.BLACK);
+        Border borderboton = BorderFactory.createLineBorder(Color.GREEN.darker().darker(), 2);
+        setBorder(borderboton);
+        return icon8Player1Button;
+    }
+    public JButton makeIcon9Player1Button(){
+        ImageIcon icon9 = new ImageIcon("imagenes/iconosJugador1.png");
+        icon9Player1Button = new CreateButtons("Icono 9", icon9);
+        setBackground(Color.YELLOW.darker());
+        setForeground(Color.BLACK);
+        Border borderboton = BorderFactory.createLineBorder(Color.GREEN.darker().darker(), 2);
+        setBorder(borderboton);
+        return icon9Player1Button;
+    }
+    public JButton makeIcon10Player1Button(){
+        ImageIcon icon10 = new ImageIcon("imagenes/iconosJugador1.png");
+        icon10Player1Button = new CreateButtons("Icono 10", icon10);
+        setBackground(Color.YELLOW.darker());
+        setForeground(Color.BLACK);
+        Border borderboton = BorderFactory.createLineBorder(Color.GREEN.darker().darker(), 2);
+        setBorder(borderboton);
+        return icon10Player1Button;
     }
 
 
