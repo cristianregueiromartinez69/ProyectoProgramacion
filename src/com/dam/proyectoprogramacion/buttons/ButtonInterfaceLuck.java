42afa8f291509a452b8da50a6a47b7d166ab3944
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-04-29 17:16:52 +0200

    añadiendo imagenes de cara o cruz al archivo imagenes

--- src/com/dam/proyectoprogramacion/buttons/ButtonInterfaceLuck.java
+++ src/com/dam/proyectoprogramacion/buttons/ButtonInterfaceLuck.java
@@ -1,4 +1,37 @@
 package com.dam.proyectoprogramacion.buttons;
 
-public class ButtonInterfaceLuck {
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class ButtonInterfaceLuck extends JPanel {
+
+    private JButton faceButton;
+    private JButton tailButton;
+
+    public  JButton makeAccountButton(){
+
+        ImageIcon iconAccount = new ImageIcon("imagenes/crearCuenta.png");
+        createAccountButton = new CreateButtons("Crear Cuenta", iconAccount);
+        setBackground(Color.ORANGE);
+        setForeground(Color.BLACK);
+        setPreferredSize(new Dimension(150,50));
+        Border borderboton = BorderFactory.createLineBorder(Color.YELLOW, 2);
+        setBorder(borderboton);
+        return createAccountButton;
+    }
+
+    public  JButton makeBattleButton(){
+
+        ImageIcon iconBattle = new ImageIcon("imagenes/iconoBatalla.png");
+        combatButton = new CreateButtons("Batalla", iconBattle);
+        setBackground(Color.WHITE);
+        setForeground(Color.BLACK);
+        setPreferredSize(new Dimension(150,50));
+        Border borderboton = BorderFactory.createLineBorder(Color.YELLOW, 2);
+        setBorder(borderboton);
+        return combatButton;
+    }
+
+
 }
