78a0229b949771791f5b9ef035c4a7ac3d87698b
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-04-30 19:52:30 +0200

    javadoc del metodo de logo segun el tipo del pokemon

--- src/com/dam/proyectoprogramacion/methodsandmain/MethodsInterfaceAttacks.java
+++ src/com/dam/proyectoprogramacion/methodsandmain/MethodsInterfaceAttacks.java
@@ -2,8 +2,16 @@ package com.dam.proyectoprogramacion.methodsandmain;
 
 import javax.swing.*;
 
+/**
+ * clase con los metodos que tendrá el panel de ataques pokemon
+ * @author cristian
+ * @version v1.0
+ */
 public class MethodsInterfaceAttacks {
 
+    /**
+     * atributos estaticos finales de tipo String que son los tipos de los pokemon
+     */
     private static final String NORMAL = "normal";
     private static final String FIRE = "fuego";
     private static final String WATER = "agua";
@@ -24,6 +32,11 @@ public class MethodsInterfaceAttacks {
     private static final String FAIRY = "hada";
 
 
+    /**
+     * metodo para establecer el logo de los ataques de los pokemon según el tipo
+     * @param type el tipo del ataque
+     * @return el imaIcon con el icono correspondiente al tipo
+     */
     public static ImageIcon putImageTypePokemon(String type) {
         ImageIcon iconType;
         switch (type) {
@@ -99,6 +112,9 @@ public class MethodsInterfaceAttacks {
                 iconType = new ImageIcon("imagenes/hada.png");
                 break;
             }
+            /**
+             * si no es ningun tipo, devolverá null
+             */
             default -> {
                 iconType = null;
 
