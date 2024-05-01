19b99bf8c6d97a13e66182a7ff8ed414e97d0edc
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-01 11:23:59 +0200

    javadoc de la clase con los botones de cambiar de pokemon

--- src/com/dam/proyectoprogramacion/buttons/ButtonInterfaceChangePokemon.java
+++ src/com/dam/proyectoprogramacion/buttons/ButtonInterfaceChangePokemon.java
@@ -6,13 +6,25 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
 
+/**
+ * clase con los botones de la interfaz de cambios de pokemons
+ * @author cristian
+ * @version v1.0
+ */
 public class ButtonInterfaceChangePokemon extends JPanel {
 
+    /**
+     * atributos privados de tipo JButton que son los 3 miembros del equipo y un boton para volver al combate
+     */
     private JButton pokemon1team;
     private JButton pokemon2team;
     private JButton pokemon3team;
     private JButton back;
 
+    /**
+     * metodo para crear el boton del primer pokemon del equipo
+     * @return el boton creado
+     */
     public JButton makePokemon1TeamButton(){
         ImageIcon iconGengar = MethodsInterfaceChangePokemon.getImagePokemonTeam1("gengar");
         pokemon1team = new CreateButtons(MethodsInterfaceChangePokemon.getNameChangePokemon1Team("gengar"), iconGengar);
@@ -22,6 +34,10 @@ public class ButtonInterfaceChangePokemon extends JPanel {
         setBorder(borderboton);
         return pokemon1team;
     }
+    /**
+     * metodo para crear el boton del segundo pokemon del equipo
+     * @return el boton creado
+     */
     public JButton makePokemon2TeamButton(){
         ImageIcon iconMilotic = MethodsInterfaceChangePokemon.getImagePokemonTeam2("milotic");
         pokemon2team = new CreateButtons(MethodsInterfaceChangePokemon.getNameChangePokemon1Team("milotic"), iconMilotic);
@@ -31,6 +47,10 @@ public class ButtonInterfaceChangePokemon extends JPanel {
         setBorder(borderboton);
         return pokemon2team;
     }
+    /**
+     * metodo para crear el boton del tercer pokemon del equipo
+     * @return el boton creado
+     */
     public JButton makePokemon3TeamButton(){
         ImageIcon iconSnorlax = MethodsInterfaceChangePokemon.getImagePokemonTeam3("snorlax");
         pokemon3team = new CreateButtons(MethodsInterfaceChangePokemon.getNameChangePokemon1Team("snorlax"), iconSnorlax);
@@ -40,6 +60,10 @@ public class ButtonInterfaceChangePokemon extends JPanel {
         setBorder(borderboton);
         return pokemon3team;
     }
+    /**
+     * metodo para crear el boton de volver al combate pokemon en caso de que se quiera cambiar de estrategia
+     * @return el boton creado
+     */
     public JButton makeBackButton(){
         ImageIcon iconBack = new ImageIcon("imagenes/backMenu.png");
         back = new CreateButtons("volver", iconBack);
