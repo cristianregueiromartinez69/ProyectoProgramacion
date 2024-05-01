ae02a13a863d9571f1be4e5145fc71645e88b5c2
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-01 11:54:24 +0200

    javadoc de los metodos de crear botones del salon de la fama

--- src/com/dam/proyectoprogramacion/buttons/ButtonInterfaceSkipHall.java
+++ src/com/dam/proyectoprogramacion/buttons/ButtonInterfaceSkipHall.java
@@ -4,12 +4,24 @@ import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
 
+/**
+ * clase con los botones de la interfaz del salon de la fama
+ * @author cristian
+ * @version v1.0
+ */
 public class ButtonInterfaceSkipHall extends JPanel {
 
+    /**
+     * atributos privados de tipo JButton con los botones de la interfaz del salon de la fama
+     */
     private JButton playAgainButton;
     private JButton backToMenuButton;
 
 
+    /**
+     * metodo para crear el boton de otra batalla
+     * @return el boton creado
+     */
     public JButton makePlayAgainButton(){
         ImageIcon iconBack = new ImageIcon("imagenes/iconoBatalla.png");
         playAgainButton = new CreateButtons("otra batalla", iconBack);
@@ -20,6 +32,10 @@ public class ButtonInterfaceSkipHall extends JPanel {
         return playAgainButton;
     }
 
+    /**
+     * metodo para crear el boton de volver al menu
+     * @return el boton creado
+     */
     public JButton makeBackButton(){
         ImageIcon iconBack = new ImageIcon("imagenes/backMenu.png");
         backToMenuButton = new CreateButtons("ir la menu", iconBack);
