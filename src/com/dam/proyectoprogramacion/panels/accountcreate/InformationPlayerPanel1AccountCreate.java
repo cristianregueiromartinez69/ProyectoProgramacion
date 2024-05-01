3ed01cdc6c4a757b6f1e9c83256aeabcd50b789e
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-01 14:27:56 +0200

    javadoc del panel de la informacion del jugador 1 en la interfaz de crear cuenta

--- src/com/dam/proyectoprogramacion/panels/accountcreate/InformationPlayerPanel1AccountCreate.java
+++ src/com/dam/proyectoprogramacion/panels/accountcreate/InformationPlayerPanel1AccountCreate.java
@@ -3,26 +3,36 @@ package com.dam.proyectoprogramacion.panels.accountcreate;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * clase que extiende de JPanel que servirá para la información del jugador 1
+ * @author cristian
+ * @version v1.0
+ */
 public class InformationPlayerPanel1AccountCreate extends JPanel {
 
+    /**
+     * atributos privados de tipo JLabel y JTextfield para introducir un alias
+     */
     private JLabel player1Label;
     private JLabel indicatorPutALiasLabel;
     private JTextField aliasTextPlayer1;
 
 
+    /**
+     * constructor en el que se inician las variables y se establece un layout
+     * el layout será de tipo BoxLayout con una disposición en vertical
+     */
     public InformationPlayerPanel1AccountCreate() {
 
         setVisible(true);
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(20,0,20,0);
         player1Label = new JLabel("Jugador 1");
         player1Label.setForeground(Color.blue);
-        this.add(player1Label, gbc);
+        this.add(player1Label);
         indicatorPutALiasLabel = new JLabel("Introduce un alias");
-        this.add(indicatorPutALiasLabel, gbc);
+        this.add(indicatorPutALiasLabel);
         aliasTextPlayer1 = new JTextField(5);
-        this.add(aliasTextPlayer1, gbc);
+        this.add(aliasTextPlayer1);
 
     }
 }
