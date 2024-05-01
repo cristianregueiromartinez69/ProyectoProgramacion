a9c93dae0abee4cf6fcdb68f6411cfce9952f54e
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-01 14:24:43 +0200

    panel de informacion del jugador 1 funcionando correctamnete, faltan margenes

--- src/com/dam/proyectoprogramacion/panels/accountcreate/InformationPlayerPanel1AccountCreate.java
+++ src/com/dam/proyectoprogramacion/panels/accountcreate/InformationPlayerPanel1AccountCreate.java
@@ -14,13 +14,15 @@ public class InformationPlayerPanel1AccountCreate extends JPanel {
 
         setVisible(true);
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(20,0,20,0);
         player1Label = new JLabel("Jugador 1");
         player1Label.setForeground(Color.blue);
-        this.add(player1Label);
+        this.add(player1Label, gbc);
         indicatorPutALiasLabel = new JLabel("Introduce un alias");
-        this.add(indicatorPutALiasLabel);
+        this.add(indicatorPutALiasLabel, gbc);
         aliasTextPlayer1 = new JTextField(5);
-        this.add(aliasTextPlayer1);
+        this.add(aliasTextPlayer1, gbc);
 
     }
 }
