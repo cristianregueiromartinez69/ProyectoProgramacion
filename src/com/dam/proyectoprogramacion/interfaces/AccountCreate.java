bab9fdbb1d6bbe169bccd5d6bf2c3eda90146640
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-01 14:33:09 +0200

    javadoc de la interfaz de crear cuenta (primeros paneles)

--- src/com/dam/proyectoprogramacion/interfaces/AccountCreate.java
+++ src/com/dam/proyectoprogramacion/interfaces/AccountCreate.java
@@ -6,29 +6,56 @@ import com.dam.proyectoprogramacion.panels.accountcreate.*;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * clase con la interfaz de crear cuenta en la que nos loguearemos para poder jugar
+ *
+ * @author cristian
+ * @version v1.0
+ */
 public class AccountCreate extends JFrame {
 
+    /**
+     * atributos privados de tipo JPanel para dividir la informacion del jugador 1 y el jugador 2
+     */
     private JPanel globalPlayerPanel1;
     private JPanel globalPlayerPanel2;
 
+    /**
+     * atributos privados de tipo JPanel para poner la información del jugador 1 y del jugador 2
+     */
     private JPanel informationPlayerPanel1;
     private JPanel informationPLayerPanel2;
 
+    /**
+     * atributos privados de tipo JPanel para poner los botones de listo del jugador 1 y del jugador 2
+     */
     private JPanel confirmAreaPlayer1;
     private JPanel ConfirmAreaPlayer2;
 
+    /**
+     * atributo privado de tipo JPanel para poner el boton de start de ambos jugadores cuando terminaron de crear la cuenta
+     */
     private JPanel startAreaPanel;
 
-    public AccountCreate(){
+    public AccountCreate() {
+        /**
+         * le damos visibilidad y ponemos un titulo al frame
+         */
         setTitle("Crear una cuenta");
         setSize(600, 400);
         setVisible(true);
 
+        /**
+         * varibale de tipo imagen que tiene el path de la imagen de fondo
+         */
         Image backgroundImage = new ImageIcon("imagenes/fondosPreparacion.png").getImage();
 
         BackgroundMainMenuPanel backgroundPanel = new BackgroundMainMenuPanel(backgroundImage);
         backgroundPanel.setLayout(new BorderLayout());
 
+        /**
+         * creamos un jpanel que es igual a un metodo que nos vuelve un jpanel
+         */
         JPanel contentPanel = makeContentPanel();
         GridBagConstraints gbc = new GridBagConstraints();
 
@@ -37,19 +64,31 @@ public class AccountCreate extends JFrame {
 
         add(backgroundPanel);
 
+        /**
+         * iniciamos el panel global de jugador 1
+         */
         globalPlayerPanel1 = new JPanel();
         globalPlayerPanel1.setLayout(new GridBagLayout());
 
         gbc.gridx = 0;
         gbc.gridy = 0;
-        gbc.insets = new Insets(20,20,20,20);
+        gbc.insets = new Insets(20, 20, 20, 20);
 
+        /**
+         * lo añadimos al panel que tiene la imagen
+         * definimos un nuevo panel que es igual a una clase que extiende de jpanel
+         */
         contentPanel.add(globalPlayerPanel1, gbc);
         JPanel informationPanelPlayer1 = new InformationPlayerPanel1AccountCreate();
         globalPlayerPanel1.add(informationPanelPlayer1);
 
 
     }
+
+    /**
+     * metodo para construir un panel
+     * @return el panel creado
+     */
 
     private JPanel makeContentPanel() {
         JPanel contentPanel = new JPanel();
