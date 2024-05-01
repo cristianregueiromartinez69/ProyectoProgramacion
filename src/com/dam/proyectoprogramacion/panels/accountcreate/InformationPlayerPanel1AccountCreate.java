5be880a85b30548032e65a4478657ae28cfd2872
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-01 14:13:13 +0200

    haciendo panel de información del jugador 1 en la interfaz de crear cuenta

--- src/com/dam/proyectoprogramacion/panels/accountcreate/InformationPlayerPanel1AccountCreate.java
+++ src/com/dam/proyectoprogramacion/panels/accountcreate/InformationPlayerPanel1AccountCreate.java
@@ -1,4 +1,26 @@
 package com.dam.proyectoprogramacion.panels.accountcreate;
 
-public class InformationPlayerPanel1AccountCreate {
+import javax.swing.*;
+import java.awt.*;
+
+public class InformationPlayerPanel1AccountCreate extends JPanel {
+
+    private JLabel player1Label;
+    private JLabel indicatorPutALiasLabel;
+    private JTextField aliasTextPlayer1;
+
+
+    public InformationPlayerPanel1AccountCreate() {
+
+        setVisible(true);
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        player1Label = new JLabel("Jugador 1");
+        player1Label.setForeground(Color.blue);
+        this.add(player1Label);
+        indicatorPutALiasLabel = new JLabel("Introduce un alias");
+        this.add(indicatorPutALiasLabel);
+        aliasTextPlayer1 = new JTextField(5);
+        this.add(aliasTextPlayer1);
+
+    }
 }
