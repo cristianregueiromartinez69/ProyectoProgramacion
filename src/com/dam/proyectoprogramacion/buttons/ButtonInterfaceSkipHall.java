15827745757f9413aa94ac26d5534157bef963ed
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-01 11:38:00 +0200

    haciendo botones del salon de la fama

--- src/com/dam/proyectoprogramacion/buttons/ButtonInterfaceSkipHall.java
+++ src/com/dam/proyectoprogramacion/buttons/ButtonInterfaceSkipHall.java
@@ -1,4 +1,23 @@
 package com.dam.proyectoprogramacion.buttons;
 
-public class ButtonInterfaceSkipHall {
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class ButtonInterfaceSkipHall extends JPanel {
+
+    private JButton playAgainButton;
+    private JButton backToMenuButton;
+
+
+
+    public JButton makeBackButton(){
+        ImageIcon iconBack = new ImageIcon("imagenes/backMenu.png");
+        backToMenuButton = new CreateButtons("ir la menu", iconBack);
+        setBackground(Color.BLUE);
+        setForeground(Color.WHITE);
+        Border borderboton = BorderFactory.createLineBorder(Color.BLACK, 2);
+        setBorder(borderboton);
+        return backToMenuButton;
+    }
 }
