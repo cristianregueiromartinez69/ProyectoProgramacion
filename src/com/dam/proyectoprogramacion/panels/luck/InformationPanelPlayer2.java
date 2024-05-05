f2277e6137d6d2bff4960af0f5f47b6c98b4b203
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-05 15:58:00 +0200

    añadiendo imagen y clase con background de la interfaz luck

--- src/com/dam/proyectoprogramacion/panels/luck/InformationPanelPlayer2.java
+++ src/com/dam/proyectoprogramacion/panels/luck/InformationPanelPlayer2.java
@@ -1,4 +1,28 @@
 package com.dam.proyectoprogramacion.panels.luck;
 
-public class InformationPanelPlayer2 {
+import javax.swing.*;
+import java.awt.*;
+
+public class InformationPanelPlayer2 extends JPanel {
+    private JLabel player2Label;
+    private JLabel indicatorPutALiasLabel;
+    private static JTextField aliasTextPlayer2;
+    private JButton enterButtonPlayer2;
+
+    public InformationPanelPlayer2(){
+        setVisible(true);
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        player2Label = new JLabel("Jugador 2");
+        player2Label.setForeground(Color.blue);
+        this.add(player2Label);
+        indicatorPutALiasLabel = new JLabel("Introduce un alias");
+        this.add(indicatorPutALiasLabel);
+        aliasTextPlayer2 = new JTextField(5);
+        this.add(aliasTextPlayer2);
+        enterButtonPlayer2 = new JButton("enter");
+        enterButtonPlayer2.setBackground(Color.GREEN);
+        enterButtonPlayer2.setForeground(Color.WHITE);
+        enterButtonPlayer2.setPreferredSize(new Dimension(100,100));
+        this.add(enterButtonPlayer2);
+    }
 }
