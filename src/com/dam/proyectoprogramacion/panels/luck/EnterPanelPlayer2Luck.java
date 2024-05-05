d78f0504b877c84f554ee8eedde732d64dd4cb93
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-05 16:35:34 +0200

    javadoc de la clase EnterPanelPlayer2Luck

--- src/com/dam/proyectoprogramacion/panels/luck/EnterPanelPlayer2Luck.java
+++ src/com/dam/proyectoprogramacion/panels/luck/EnterPanelPlayer2Luck.java
@@ -2,10 +2,20 @@ package com.dam.proyectoprogramacion.panels.luck;
 
 import javax.swing.*;
 import java.awt.*;
-
+/**
+ * clase que tiene el panel de informacion del jugador 2
+ * @author cristian
+ * @version v1.0
+ */
 public class EnterPanelPlayer2Luck extends JPanel {
+    /**
+     * atributo de tipo jButton que será el boton de enter
+     */
     private JButton enterButtonPlayer2;
     public EnterPanelPlayer2Luck(){
+        /**
+         * iniciamos el boton y lo añadimos al panel
+         */
         enterButtonPlayer2 = new JButton("enter");
         enterButtonPlayer2.setBackground(Color.GREEN);
         enterButtonPlayer2.setForeground(Color.WHITE);
