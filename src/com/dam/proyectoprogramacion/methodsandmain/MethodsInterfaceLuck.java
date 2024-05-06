7d17d72892205a299819a88fd4f6526e992b1f76
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-06 11:17:15 +0200

    javadoc de los metodos de la interfaz luck

--- src/com/dam/proyectoprogramacion/methodsandmain/MethodsInterfaceLuck.java
+++ src/com/dam/proyectoprogramacion/methodsandmain/MethodsInterfaceLuck.java
@@ -6,13 +6,29 @@ import com.dam.proyectoprogramacion.panels.luck.InformationPanelPlayer1Luck;
 
 import java.util.Map;
 
+/**
+ * clase con los metodos de la interfaz luck
+ * @author cristian
+ * 2version v2.0
+ */
 public class MethodsInterfaceLuck {
 
+    /**
+     * metodo para crear un panel en función del alias escrito
+     * @return el panel creado
+     */
     public static JPanel selectAliasImagePlayer1(){
         JPanel aliasPlayer1 = new JPanel();
         aliasPlayer1.setLayout(new BoxLayout(aliasPlayer1, BoxLayout.Y_AXIS));
+        /**
+         * variable de tipo string que es igual a lo introducido en el jtexfield de la intefaz luck
+         */
         String aliasToLookFor = InformationPanelPlayer1Luck.getAliasTextPlayer1().getText();
         ImageIcon imageAlias;
+        /**
+         * este for recorre le hashmao de los juagdores
+         * si coincide con uno existente, se crea el panel con el alias y la foto escogida
+         */
         for (Map.Entry<String, ImageIcon> entry : MethodsInterfaceAccountCreate.getPlayers().entrySet()) {
             if (entry.getKey().equals(aliasToLookFor)) {
                     JLabel aliasText = new JLabel(aliasToLookFor);
@@ -26,7 +42,11 @@ public class MethodsInterfaceLuck {
         return aliasPlayer1;
         }
 
-        public static boolean checkAliasPlayer1(){
+    /**
+     * metodo para verificar si el usuario existe en el hashmap para el boton de enter
+     * @return true si existe y false si no existe
+     */
+    public static boolean checkAliasPlayer1(){
             String aliasToLookFor = InformationPanelPlayer1Luck.getAliasTextPlayer1().getText();
             boolean marca = false;
             for (Map.Entry<String, ImageIcon> entry : MethodsInterfaceAccountCreate.getPlayers().entrySet()) {
