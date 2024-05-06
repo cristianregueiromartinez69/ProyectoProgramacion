22a32f54209e1b3f286b488c9630d61206876a24
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-06 10:26:21 +0200

    creando clases para los alias selecionados de los jugadores en la interfaz luck

---
+++ src/com/dam/proyectoprogramacion/panels/luck/selecAliasPlayer1.java
@@ -0,0 +1,18 @@
+package com.dam.proyectoprogramacion.panels.luck;
+
+import javax.swing.*;
+
+public class selecAliasPlayer1 extends JPanel {
+
+    private JPanel aliasImagePlayer1;
+    private JPanel faceOrTailPlayer1;
+
+    public selecAliasPlayer1(){
+        setVisible(true);
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+
+
+    }
+
+}
---
+++ src/com/dam/proyectoprogramacion/panels/luck/selectAliasPlayer2.java
@@ -0,0 +1,4 @@
+package com.dam.proyectoprogramacion.panels.luck;
+
+public class selectAliasPlayer2 {
+}
