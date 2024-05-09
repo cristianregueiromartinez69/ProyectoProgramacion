e922ff86109cecf53fada04c1e1a1c2ad23aea3b
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-09 18:54:45 +0200

    añadiendo nuevas clases y hecha la clase con los JLabels de los pokemons para la interfaz seleccion de pokemons

---
+++ src/com/dam/proyectoprogramacion/panels/selectionpokemon/JLabelsPokemons.java
@@ -0,0 +1,107 @@
+package com.dam.proyectoprogramacion.panels.selectionpokemon;
+
+import javax.swing.*;
+
+public class JLabelsPokemons {
+
+    private static JLabel drampaLabel;
+    private static JLabel empoleonLabel;
+    private static JLabel garchompLabel;
+    private static JLabel gengarLabel;
+    private static JLabel infernapeLabel;
+    private static JLabel lucarioLabel;
+    private static JLabel luxrayLabel;
+    private static JLabel marshadowLabel;
+    private static JLabel miloticLabel;
+    private static JLabel snorlaxLabel;
+    private static JLabel tinkatonLabel;
+    private static JLabel tyranitarLabel;
+
+    public JLabel makeDrampaLabel(){
+        ImageIcon imageIcon = new ImageIcon("imagenes/drampaSP.png");
+        JLabel drampa = new JLabel();
+        drampa.setIcon(imageIcon);
+        return drampa;
+
+    }
+    public JLabel makeEmpoleonLabel(){
+        ImageIcon imageIcon = new ImageIcon("imagenes/empoleonSP.png");
+        JLabel empoleon = new JLabel();
+        empoleon.setIcon(imageIcon);
+        return empoleon;
+
+    }
+    public JLabel makeGarchompLabel(){
+        ImageIcon imageIcon = new ImageIcon("imagenes/garchompSP.png");
+        JLabel garchomp = new JLabel();
+        garchomp.setIcon(imageIcon);
+        return garchomp;
+
+    }
+    public JLabel makeGengarLabel(){
+        ImageIcon imageIcon = new ImageIcon("imagenes/gengarSP.png");
+        JLabel gengar = new JLabel();
+        gengar.setIcon(imageIcon);
+        return gengar;
+
+    }
+    public JLabel makeInfernapeLabel(){
+        ImageIcon imageIcon = new ImageIcon("imagenes/infernapeSP.png");
+        JLabel infernape = new JLabel();
+        infernape.setIcon(imageIcon);
+        return infernape;
+
+    }
+    public JLabel makeLucarioLabel(){
+        ImageIcon imageIcon = new ImageIcon("imagenes/lucarioSP.png");
+        JLabel lucario = new JLabel();
+        lucario.setIcon(imageIcon);
+        return lucario;
+
+    }
+    public JLabel makeLuxrayLabel(){
+        ImageIcon imageIcon = new ImageIcon("imagenes/luxraySP.png");
+        JLabel luxray = new JLabel();
+        luxray.setIcon(imageIcon);
+        return luxray;
+
+    }
+    public JLabel makeMarshadowLabel(){
+        ImageIcon imageIcon = new ImageIcon("imagenes/marshadowSP.png");
+        JLabel marshadow = new JLabel();
+        marshadow.setIcon(imageIcon);
+        return marshadow;
+
+    }
+    public JLabel makeMiloticLabel(){
+        ImageIcon imageIcon = new ImageIcon("imagenes/miloticSP.png");
+        JLabel milotic = new JLabel();
+        milotic.setIcon(imageIcon);
+        return milotic;
+
+    }
+    public JLabel makeSnorlaxLabel(){
+        ImageIcon imageIcon = new ImageIcon("imagenes/snorlaxSP.png");
+        JLabel snorlax = new JLabel();
+        snorlax.setIcon(imageIcon);
+        return snorlax;
+
+    }
+    public JLabel makeTinkatonLabel(){
+        ImageIcon imageIcon = new ImageIcon("imagenes/tinkatonSP.png");
+        JLabel tinkaton = new JLabel();
+        tinkaton.setIcon(imageIcon);
+        return tinkaton;
+
+    }
+    public JLabel makeTyranitarLabel(){
+        ImageIcon imageIcon = new ImageIcon("imagenes/tyranitarSP.png");
+        JLabel tyranitar = new JLabel();
+        tyranitar.setIcon(imageIcon);
+        return tyranitar;
+
+    }
+
+
+
+}
---
+++ src/com/dam/proyectoprogramacion/panels/selectionpokemon/PanelsPokemonsToSelect.java
@@ -0,0 +1,8 @@
+package com.dam.proyectoprogramacion.panels.selectionpokemon;
+
+import javax.swing.*;
+
+public class PanelsPokemonsToSelect extends JPanel {
+
+
+}
