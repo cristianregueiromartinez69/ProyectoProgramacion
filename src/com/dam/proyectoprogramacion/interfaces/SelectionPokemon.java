34c5a8d0acc37371d1cbf278495e33fa0c55db8e
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-09 18:31:01 +0200

    reutilizando codigo para la imagen de fondo de la interfaz seleccion de pokemon

--- src/com/dam/proyectoprogramacion/interfaces/SelectionPokemon.java
+++ src/com/dam/proyectoprogramacion/interfaces/SelectionPokemon.java
@@ -3,4 +3,16 @@ package com.dam.proyectoprogramacion.interfaces;
 import javax.swing.*;
 
 public class SelectionPokemon extends JFrame {
+
+    private JPanel globalPanelPlayer1;
+    private JPanel globalPanelSelectionPokemon;
+    private JPanel globalPanelPlayer2;
+    private JPanel goToBattlePanel;
+
+    public SelectionPokemon(){
+        setVisible(true);
+        setBounds(600,600,600,600);
+
+    }
+
 }
