31722dcfe339a84d41449a211108c80bd012e037
Chema <jcaobaqueiro@danielcastelao.org>
2024-05-10 10:51:09 +0200

    Metodo que se va a utilizar posteriormente para insertar los datos de cada pokemon

--- src/com/dam/proyectoprogramacion/bbdd/Conexion.java
+++ src/com/dam/proyectoprogramacion/bbdd/Conexion.java
@@ -31,9 +31,28 @@ public class Conexion {
 
         try (Statement stmt = con.createStatement()){
                 String sql = String.join("\n", "CREATE TABLE IF NOT EXISTS  Pokemon (", "Name VARCHAR(12) NOT NULL, PRIMARY KEY", "Type VARCHAR(10) NOT NULL, ", "Level INT NOT NULL, ", "Sex VARCHAR(7) NOT NULL, ", "Speed INT NOT NULL, ", "Lyfe INT NOT NULL, ", "Syze FLOAT NOT NULL, ", "Weight FLOAT NOT NULL, ", "Attacks VARCHAR(20) NOT NULL, ");
-
+                stmt.executeUpdate(sql);
+                JOptionPane.showMessageDialog(null, "Tabla de Pokemon registrado con exito");
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            JOptionPane.showMessageDialog(null,"Error al crear la tabla");
+        }
+    }
+
+    public void InsertarPokemon(String Name, String Type, Integer Level, Integer Life, String Sex, Integer Speed, Float Syze, Float Weight, String Atacks) {
+        String sql = "INSERT INTO Pokemon (Name, Type, Level, Life, Sex, Speed. Syze, Weight, Attacks) VALUES (?,?,?,?,?,?,?,?,?)";
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+            pstmt.setString(1,Name);
+            pstmt.setString(1,Type);
+            pstmt.setInt(1,Level);
+            pstmt.setInt(1,Life);
+            pstmt.setString(1,Sex);
+            pstmt.setInt(1,Speed);
+            pstmt.setFloat(1,Syze);
+            pstmt.setFloat(1,Weight);
+            pstmt.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Insertado exitosamente");
+        }catch (SQLException e) {
+            JOptionPane.showMessageDialog(null,"Error al insertar la tabla");
         }
     }
 
