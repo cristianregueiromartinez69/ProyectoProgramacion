9b90f9c5b8ffe0efe80bedcb8be0cf882a223351
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-11 13:51:45 +0200

    reutilizando codigo para crear el  panel para el boton de finish del jugador 2 en la interfaz de seleccion de pokemon

--- src/com/dam/proyectoprogramacion/panels/selectionpokemon/EnterPanelPlayer2.java
+++ src/com/dam/proyectoprogramacion/panels/selectionpokemon/EnterPanelPlayer2.java
@@ -1,4 +1,23 @@
 package com.dam.proyectoprogramacion.panels.selectionpokemon;
+import com.dam.proyectoprogramacion.buttons.*;
 
-public class EnterPanelPlayer2 {
+import javax.swing.*;
+import java.awt.*;
+
+public class EnterPanelPlayer2 extends JPanel {
+
+    private static JButton enterButtonPlayer2;
+
+    public EnterPanelPlayer2(){
+        setVisible(true);
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        enterButtonPlayer2 = new ButtonInterfacePokemonSelect().makeReadyPlayer2Button();
+        enterButtonPlayer2.setPreferredSize(new Dimension(150,40));
+        enterButtonPlayer2.setForeground(Color.WHITE);
+        this.add(enterButtonPlayer2);
+    }
+
+    public static JButton getEnterButtonPlayer2() {
+        return enterButtonPlayer2;
+    }
 }
