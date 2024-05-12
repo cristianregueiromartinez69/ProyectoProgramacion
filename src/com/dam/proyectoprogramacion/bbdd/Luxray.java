dad8b19b0e099b47eccd6bcee0d9b42c7742e7c8
Chema <jcaobaqueiro@danielcastelao.org>
2024-05-12 17:45:46 +0200

    Creacion del pokemon Luxray con sus respectivos datos en cada tabla

---
+++ src/com/dam/proyectoprogramacion/bbdd/Luxray.java
@@ -0,0 +1,16 @@
+package com.dam.proyectoprogramacion.bbdd;
+
+public class Luxray {
+
+    public void crearPokemon(){
+        Conexion datos = new Conexion();
+        datos.crearConexion();
+        datos.InsertarPokemon("Luxray","Electrico","",100,301,"Male",176,1.4F,42f,"Luxray");
+        Atacks ataques = new Atacks();
+        ataques.InsertAtacks("Mordisco","Siniestro",60,25,100f);
+        ataques.InsertAtacks("Voltio Cruel","Electrico",90,15,100f);
+        ataques.InsertAtacks("Colmillo Rayo","Electrico",65,15,95f);
+        ataques.InsertAtacks("Psicocolmillo","Psiquico",85,10,100f);
+    }
+
+}
