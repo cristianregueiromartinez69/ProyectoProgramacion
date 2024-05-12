8b06c285302a59bd916bf2a37f19747889d9b203
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-12 16:43:11 +0200

    metodos de la interfaz de seleccion de pokemons hechos y funcionando #	imagenes/tick.png #	src/com/dam/proyectoprogramacion/bbdd/Conexion.java #	src/com/dam/proyectoprogramacion/interfaces/Luck.java #	src/com/dam/proyectoprogramacion/interfaces/SelectionPokemon.java #	src/com/dam/proyectoprogramacion/methodsandmain/Main.java #	src/com/dam/proyectoprogramacion/methodsandmain/MethosInterfaceSelectionPokemon.java #	src/com/dam/proyectoprogramacion/panels/selectionpokemon/PanelPlayer1.java #	src/com/dam/proyectoprogramacion/panels/selectionpokemon/PanelPlayer2.java #	src/com/dam/proyectoprogramacion/pokemon/Empoleon.java #	src/com/dam/proyectoprogramacion/pokemon/Garchomp.java #	src/com/dam/proyectoprogramacion/pokemon/PokemonProperties.java

---
+++ src/com/dam/proyectoprogramacion/panels/selectionpokemon/GoToBattlePanel.java
@@ -0,0 +1,36 @@
+package com.dam.proyectoprogramacion.panels.selectionpokemon;
+
+import com.dam.proyectoprogramacion.buttons.ButtonInterfacePokemonSelect;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * clase con el panel para poner el boton de ir a la batalla
+ * @author cristian
+ * @version v2.0
+ */
+public class GoToBattlePanel extends JPanel {
+
+    /**
+     * constructor de la clase
+     */
+    public GoToBattlePanel(){
+        /**
+         * lo hacemos visible y le damos un layout
+         */
+        setVisible(true);
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        /**
+         * instanciamos un jButton igual a un metodo que crea el boton
+         */
+        JButton battleButton = new ButtonInterfacePokemonSelect().makeStartButton();
+        battleButton.setBackground(Color.RED);
+        battleButton.setForeground(Color.WHITE);
+        /**
+         * lo añadimos
+         */
+        this.add(battleButton);
+
+    }
+}
