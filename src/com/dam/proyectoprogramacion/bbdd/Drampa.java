6e174460602fc95180092753f02aba8f01cfb24e
Chema <jcaobaqueiro@danielcastelao.org>
2024-05-12 17:09:14 +0200

    Creacion del primer pokemon con sus respectivos datos en cada tabla

---
+++ src/com/dam/proyectoprogramacion/bbdd/Drampa.java
@@ -0,0 +1,17 @@
+package com.dam.proyectoprogramacion.bbdd;
+
+public class Drampa {
+    public void crearPokemon(){
+        Conexion datos = new Conexion();
+        datos.crearConexion();
+        datos.CrearTabla();
+        datos.InsertarPokemon("Drampa","Normal","Dragon",100,100,"Male",108,2F,75f,"Drampa");
+        Atacks ataques = new Atacks();
+        ataques.CrearTabla();
+        ataques.InsertAtacks("Pulso Dragon","Dragon",85,10,100f);
+        ataques.InsertAtacks("Hiperrayo","Normal",150,5,90f);
+        ataques.InsertAtacks("Viento Hielo","Hielo",55,15,95f);
+        ataques.InsertAtacks("Rayo","Eléctrico",90,15,100f);
+    }
+
+}
