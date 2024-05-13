889023af117e90e340a854c17a907a78103a233a
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-13 18:26:24 +0200

    javadoc de los ataques del jugador 1 en batalla

--- src/com/dam/proyectoprogramacion/panels/battle/battleplayer1/AttacksPokemonPlayer1.java
+++ src/com/dam/proyectoprogramacion/panels/battle/battleplayer1/AttacksPokemonPlayer1.java
@@ -2,15 +2,29 @@ package com.dam.proyectoprogramacion.panels.battle.battleplayer1;
 
 import javax.swing.*;
 
+/**
+ * clase con los paneles de los ataques del pokemon en batalla
+ * @author cristian
+ * @version v3.0
+ */
 public class AttacksPokemonPlayer1 extends JPanel {
 
+    /**
+     * atributos privados de la clase
+     */
     private JPanel row1Of2PokemonAttacksInBattle;
     private JPanel row2Of2PokemonAttacksInBattle;
 
     public AttacksPokemonPlayer1(){
+        /**
+         * hacemos el panel visible y le damos un layout
+         */
       setVisible(true);
       setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
+        /**
+         * iniciamos el panel y añadimos el panel de otra clase
+         */
       row1Of2PokemonAttacksInBattle = new JPanel();
       row1Of2PokemonAttacksInBattle.setLayout(new BoxLayout(row1Of2PokemonAttacksInBattle, BoxLayout.X_AXIS));
       row1Of2PokemonAttacksInBattle.add(new OneAndTwoAttacksPokemonInBattlePLayer1());
--- src/com/dam/proyectoprogramacion/panels/battle/battleplayer1/OneAndTwoAttacksPokemonInBattlePLayer1.java
+++ src/com/dam/proyectoprogramacion/panels/battle/battleplayer1/OneAndTwoAttacksPokemonInBattlePLayer1.java
@@ -6,16 +6,30 @@ import com.dam.proyectoprogramacion.methodsandmain.DataNamesIconsColorsAttacksPl
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Clase con los paneles de los ataques 1 y 2 del jugador 1
+ * @author cristian
+ * @version v3.0
+ */
 public class OneAndTwoAttacksPokemonInBattlePLayer1 extends JPanel {
 
+    /**
+     * atributos de la clase
+     */
     private static JPanel attack1Player1;
     private static JPanel attack2Player1;
 
     public OneAndTwoAttacksPokemonInBattlePLayer1(){
 
+        /**
+         * hacemos el panel visible y le damos un layout
+         */
         setVisible(true);
         setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 
+        /**
+         * iniciamos el panel del primer ataque y metemos el boton
+         */
         attack1Player1 = new JPanel();
         JButton attack1 = new ButtonInterfaceAttacks().makeAttack1ButtonPlayer1();
         attack1.setBackground(DataNamesIconsColorsAttacksPlayer1.getColorBackgroundAttack());
