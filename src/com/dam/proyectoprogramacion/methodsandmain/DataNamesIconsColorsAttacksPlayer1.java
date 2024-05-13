4049ec35924e0a45c5ca4dd8bd88f1babde7fdfe
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-13 18:22:42 +0200

    javadoc de la clase con la informacion de ataques de pokemons

--- src/com/dam/proyectoprogramacion/methodsandmain/DataNamesIconsColorsAttacksPlayer1.java
+++ src/com/dam/proyectoprogramacion/methodsandmain/DataNamesIconsColorsAttacksPlayer1.java
@@ -2,12 +2,24 @@ package com.dam.proyectoprogramacion.methodsandmain;
 
 import java.awt.*;
 
+/**
+ * clase para almacenar datos de ataques, nombres y colores de fondo que iran cambiando en batalla
+ * @author cristian
+ * @version v3.0
+ */
 public class DataNamesIconsColorsAttacksPlayer1 {
 
+    /**
+     * atributos privados de la clase
+     */
     private static String nameAttack;
     private static String pathIconAttack;
     private static Color colorBackgroundAttack;
 
+    /**
+     * getter y setter de los atributos de la clase
+     * @return el atributo correspondiente
+     */
     public static String getNameAttack() {
         return nameAttack;
     }
