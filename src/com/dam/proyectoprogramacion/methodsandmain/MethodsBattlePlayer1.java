2b07a673221967ed0b6e235bacff7e4220b08d51
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-13 12:15:40 +0200

    javadoc del metodo que devuelve el path del pokemon del jugador 1

--- src/com/dam/proyectoprogramacion/methodsandmain/MethodsBattlePlayer1.java
+++ src/com/dam/proyectoprogramacion/methodsandmain/MethodsBattlePlayer1.java
@@ -6,22 +6,47 @@ import com.dam.proyectoprogramacion.pokemon.PokemonProperties;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * clase con los metodos para la batalla del jugador 1
+ * @author cristian
+ * @version v3.0
+ */
 public class MethodsBattlePlayer1 {
 
+    /**
+     * metodo que recorre un hashmap y busca el primer pokemon escogido por el jugador 1
+     * @param players el hashmap de los jugadores
+     * @return el path con la imagen del pokemon seleccionado
+     */
     public static String putImageAttackButtonPlayer1(HashMap<String, ArrayList<Object>> players){
 
         String pathImagePokemon="";
         String aliasPlayer1 = InformationPanelPlayer1Luck.getAliasTextPlayer1().getText();
+        /**
+         * for para recorrer el hashmap de los jugadores
+         */
         for (String key : MethosInterfaceSelectionPokemon.getPlayersBattle().keySet()) {
+            /**
+             * si encuentra la clave, entra en la condicion
+             */
             if (key.equals(aliasPlayer1)) {
+                /**
+                 * iniciamos un array de objetos y lo hacemos igual al valor del hashmap de jugadores
+                 */
                 ArrayList <Object> pokemons = MethosInterfaceSelectionPokemon.getPlayersBattle().get(key);
                 if (!pokemons.isEmpty()) {
+                    /**
+                     * hacemos un objeto de pokemons y decimos que es igual al primer elemento de la lista de pokemons
+                     */
                     PokemonProperties firstPokemon = (PokemonProperties) pokemons.get(0);
                     String name = firstPokemon.getName().toLowerCase();
                     pathImagePokemon = "imagenes/" + name + ".png";
                     break;
 
                 } else {
+                    /**
+                     * si no hay nada, devuelve null
+                     */
                     pathImagePokemon = null;
                 }
 
