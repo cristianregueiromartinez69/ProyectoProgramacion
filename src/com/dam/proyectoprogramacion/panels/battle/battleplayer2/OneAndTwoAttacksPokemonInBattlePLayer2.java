8755e9bff649df9c2b20cea2ae99e35834c8e57c
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-14 21:13:17 +0200

    reutilizando codigo para los botones de cambio de pokemon del jugador 2

--- src/com/dam/proyectoprogramacion/panels/battle/battleplayer2/OneAndTwoAttacksPokemonInBattlePLayer2.java
+++ src/com/dam/proyectoprogramacion/panels/battle/battleplayer2/OneAndTwoAttacksPokemonInBattlePLayer2.java
@@ -17,8 +17,8 @@ public class OneAndTwoAttacksPokemonInBattlePLayer2 extends JPanel {
     /**
      * atributos de la clase
      */
-    private static JPanel attack1Player1;
-    private static JPanel attack2Player1;
+    private static JPanel attack1Player2;
+    private static JPanel attack2Player2;
 
     public OneAndTwoAttacksPokemonInBattlePLayer2(){
 
@@ -31,32 +31,32 @@ public class OneAndTwoAttacksPokemonInBattlePLayer2 extends JPanel {
         /**
          * iniciamos el panel del primer ataque y metemos el boton
          */
-        attack1Player1 = new JPanel();
+        attack1Player2 = new JPanel();
         JButton attack1 = new ButtonInterfaceAttacks().makeAttack1ButtonPlayer2();
         attack1.setBackground(DataNamesIconsColorsAttacksAndPokemonsPlayer2.getColorBackgroundAttack1());
         attack1.setForeground(Color.WHITE);
         attack1.setPreferredSize(new Dimension(170,100));
         attack1.setMaximumSize(new Dimension(170,100));
         attack1.setMinimumSize(new Dimension(170,100));
-        attack1Player1.add(attack1);
+        attack1Player2.add(attack1);
 
         /**
          * iniciamos el panel del segundo ataque y metemos el boton
          */
-        attack2Player1 = new JPanel();
+        attack2Player2 = new JPanel();
         JButton attack2 = new ButtonInterfaceAttacks().makeAttack2ButtonPlayer2();
         attack2.setBackground(DataNamesIconsColorsAttacksAndPokemonsPlayer2.getColorBackgroundAttack2());
         attack2.setForeground(Color.WHITE);
         attack2.setPreferredSize(new Dimension(170,100));
         attack2.setMaximumSize(new Dimension(170,100));
         attack2.setMinimumSize(new Dimension(170,100));
-        attack2Player1.add(attack2);
+        attack2Player2.add(attack2);
 
         /**
          * lo añadimos al panel
          */
-        this.add(attack1Player1);
-        this.add(attack2Player1);
+        this.add(attack1Player2);
+        this.add(attack2Player2);
 
     }
 }
