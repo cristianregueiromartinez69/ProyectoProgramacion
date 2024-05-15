473eb3ce2263555ab45a4d1a71f30d775e152cfd
Chema <jcaobaqueiro@danielcastelao.org>
2024-05-15 12:34:47 +0200

    Javadoc de toda la clase de ataques

--- src/com/dam/proyectoprogramacion/bbdd/Atacks.java
+++ src/com/dam/proyectoprogramacion/bbdd/Atacks.java
@@ -6,6 +6,9 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ * clase en la que se va a realizar all lo relacionado con los ataques de los pokemon
+ */
 public class Atacks {
     Connection con;
 
@@ -35,11 +38,11 @@ public class Atacks {
 
     /**
      * Metodo en el que se va a introducir todos los datos de cada uno de los ataque los cuales se van a introducir
-     * @param Name
-     * @param Type
-     * @param Power
-     * @param PP
-     * @param Precission
+     * @param Name variable principal con la cual se va a identificar cada ataque
+     * @param Type tipo de cada ataque
+     * @param Power poder del ataque
+     * @param PP  cantidad de veces que se puede utilizar el ataque
+     * @param Precission  probabilidad de que acierte el ataque
      */
     public void InsertAtacks(String Name, String Type, Integer Power, Integer PP, Float Precission) {
         String sql = "INSERT INTO Ataques (Name, Type, Power, PP, Precission) VALUES (?,?,?,?,?)";
@@ -56,6 +59,10 @@ public class Atacks {
         }
     }
 
+    /**
+     * Metodo para eliminar un ataque de la base de datos
+     * @param Name nombre por el cual se va a reconocer el ataque que va a ser eliminado
+     */
     public void EliminarAtack(String Name) {
         String sql = "DELETE FROM Atacks WHERE Name = ?";
         try (PreparedStatement pstmt = con.prepareStatement(sql)){
@@ -71,6 +78,11 @@ public class Atacks {
         }
     }
 
+    /**
+     * Ḿetodo para actuazlizar el tipo de un ataque
+     * @param Name indicador de que ataque se quiere cambiar
+     * @param newType nuevo tipo al que se va a cambiar
+     */
     public void actualizarTypeAtack(String Name, String newType){
         String sql = "UPDATE Atacks SET Type = ? WHERE Name = ?";
         try (PreparedStatement pstmt = con.prepareStatement(sql)){
@@ -87,6 +99,11 @@ public class Atacks {
         }
     }
 
+    /**
+     * Metodo para modificar el poder de un ataque
+     * @param Name identificador de un ataque
+     * @param newPower nuevo valor que se va a poner en la variable power
+     */
     public void actualizarPowerAtack(String Name, Integer newPower){
         String sql = "UPDATE Atacks SET Power = ? WHERE Name = ?";
         try (PreparedStatement pstmt = con.prepareStatement(sql)){
@@ -103,6 +120,11 @@ public class Atacks {
         }
     }
 
+    /**
+     * Metodo para cambiar el numero de veces que puede ser utilizado un ataque
+     * @param Name identificador del ataque que se va a cambiar
+     * @param newPP nueva cantidad de veces que se va a poder utilizar el ataque
+     */
     public void actualizarPPAtacks(String Name, Integer newPP){
         String sql = "UPDATE Atacks SET PP = ? WHERE Name = ?";
         try (PreparedStatement pstmt = con.prepareStatement(sql)){
@@ -119,6 +141,11 @@ public class Atacks {
         }
     }
 
+    /**
+     * Metodo para actualizar la precision de un ataque
+     * @param Name identificador de un ataque
+     * @param newPrecision nueva precision que va a tener el ataque
+     */
     public void actualizarPrecissionAtacks(String Name, Float newPrecision){
         String sql = "UPDATE Atacks SET Precision = ? WHERE Name = ?";
         try (PreparedStatement pstmt = con.prepareStatement(sql)){
