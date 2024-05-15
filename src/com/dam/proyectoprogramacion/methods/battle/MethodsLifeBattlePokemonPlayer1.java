29d140452cb479c0a8f4d439d2ae13fa1439ce38
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-15 17:38:21 +0200

    metodo para obtener la vida del primer pokemon del jugador 1 en batalla

--- src/com/dam/proyectoprogramacion/methods/battle/MethodsLifeBattlePokemonPlayer1.java
+++ src/com/dam/proyectoprogramacion/methods/battle/MethodsLifeBattlePokemonPlayer1.java
@@ -1,4 +1,65 @@
 package com.dam.proyectoprogramacion.methods.battle;
 
+import com.dam.proyectoprogramacion.methods.selectionpokemon.MethosInterfaceSelectionPokemon;
+import com.dam.proyectoprogramacion.panels.luck.InformationPanelPlayer1Luck;
+import com.dam.proyectoprogramacion.pokemon.AttacksPokemons;
+import com.dam.proyectoprogramacion.pokemon.PokemonProperties;
+
+import java.util.ArrayList;
+
+/**
+ * clase con los metodos para obtener la vida de los pokemons del jugador 1
+ * @author cristian
+ * @version v3.0
+ */
 public class MethodsLifeBattlePokemonPlayer1 {
+
+    /**
+     * metodo para obtener el valor de la vida del pokemon numero 1 del jugador 2
+     *
+     * @return el valor de la vida del pokemon
+     */
+    public static int getLifePokemon1Player1() {
+
+        /**
+         * iniciamos una variable de tipo int en la que vamos a almacenar la vida del primer pokemon
+         */
+        int lifePokemon1 = 0;
+        String aliasPlayer1 = InformationPanelPlayer1Luck.getAliasTextPlayer1().getText();
+
+        /**
+         * for para recorrer el hashmap de los jugadores
+         */
+
+        for (String key : MethosInterfaceSelectionPokemon.getPlayersBattle().keySet()) {
+
+            if (key.equals(aliasPlayer1)) {
+                /**
+                 * iniciamos un array de objetos y lo hacemos igual al valor del hashmap de jugadores
+                 */
+                ArrayList<Object> pokemons = MethosInterfaceSelectionPokemon.getPlayersBattle().get(key);
+                if (!pokemons.isEmpty()) {
+
+                    /**
+                     * hacemos un objeto de pokemons y decimos que es igual al segundo elemento de la lista de pokemons
+                     */
+                    PokemonProperties firstPokemon = (PokemonProperties) pokemons.get(0);
+                    /**
+                     * almacenamos en la variable int el valor de la vida del pokemon
+                     */
+                    lifePokemon1 = firstPokemon.getLife();
+
+
+
+                    break;
+
+                }
+
+            }
+        }
+        /**
+         * retornamos la vida del pokemon
+         */
+      return lifePokemon1;
+    }
 }
---
+++ src/com/dam/proyectoprogramacion/panels/battle/battleplayer1/LifePokemonInBattlePlayer1.java
@@ -0,0 +1,21 @@
+package com.dam.proyectoprogramacion.panels.battle.battleplayer1;
+
+import javax.swing.*;
+
+public class LifePokemonInBattlePlayer1 extends JPanel {
+
+    private JPanel panelLifePokemon;
+    private JProgressBar lifePokemon;
+
+
+    public LifePokemonInBattlePlayer1(){
+
+        setVisible(true);
+        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+
+
+
+
+    }
+
+}
