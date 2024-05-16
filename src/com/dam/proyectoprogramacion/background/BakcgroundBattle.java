6394eb9774f7e7ca70cc118ebed485cf439ae080
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-16 20:56:08 +0200

    empezando a hacer la imagen de fondo de la batalla pokemon

---
+++ src/com/dam/proyectoprogramacion/background/BakcgroundBattle.java
@@ -0,0 +1,11 @@
+package com.dam.proyectoprogramacion.background;
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Clase con  imagen de fondo de la batalla pokemon
+ * @author cristian
+ * @version v3.0
+ */
+public class BakcgroundBattle {
+}
---
+++ src/com/dam/proyectoprogramacion/panels/battle/logicalbattle/PanelImageBattlePokemon.java
@@ -0,0 +1,40 @@
+package com.dam.proyectoprogramacion.panels.battle.logicalbattle;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelImageBattlePokemon extends JPanel {
+
+    private JPanel backgroundPanel;
+    private static Image pokemonPlayer1;
+    private static Image pokemonPlayer2;
+    private static Image battleScenario;
+
+    public PanelImageBattlePokemon(){
+
+    }
+
+    public static Image getPokemonPlayer1() {
+        return pokemonPlayer1;
+    }
+
+    public static void setPokemonPlayer1(Image pokemonPlayer1) {
+        PanelImageBattlePokemon.pokemonPlayer1 = pokemonPlayer1;
+    }
+
+    public static Image getPokemonPlayer2() {
+        return pokemonPlayer2;
+    }
+
+    public static void setPokemonPlayer2(Image pokemonPlayer2) {
+        PanelImageBattlePokemon.pokemonPlayer2 = pokemonPlayer2;
+    }
+
+    public static Image getBattleScenario() {
+        return battleScenario;
+    }
+
+    public static void setBattleScenario(Image battleScenario) {
+        PanelImageBattlePokemon.battleScenario = battleScenario;
+    }
+}
