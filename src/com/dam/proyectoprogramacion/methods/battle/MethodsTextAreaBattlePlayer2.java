8d2c01de8a65e3ca605b3b0d06147d1d1b06246f
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-16 19:52:45 +0200

    reutilizando codigo para el mismo metodo para el jugador 2

--- src/com/dam/proyectoprogramacion/methods/battle/MethodsTextAreaBattlePlayer2.java
+++ src/com/dam/proyectoprogramacion/methods/battle/MethodsTextAreaBattlePlayer2.java
@@ -1,6 +1,10 @@
 package com.dam.proyectoprogramacion.methods.battle;
 
 import com.dam.proyectoprogramacion.buttons.ButtonInterfaceCombat;
+import com.dam.proyectoprogramacion.methods.selectionpokemon.MethosInterfaceSelectionPokemon;
+import com.dam.proyectoprogramacion.pokemon.PokemonProperties;
+
+import java.util.ArrayList;
 
 public class MethodsTextAreaBattlePlayer2 {
 
@@ -23,6 +27,49 @@ public class MethodsTextAreaBattlePlayer2 {
 
 
         return namePokemon1Player2;
+    }
+    /**
+     * metodo para saber el tipo del pokemon que hay en batalla del jugador 2
+     * @return el tipo del pokemon
+     */
+    public static String getTypePokemonInBattlePlayer2() {
+
+        /**
+         * iniciamos distintas variables
+         * la primera almacenará el tipo
+         * la segundo la iniciamos e indicamos quees igual al nombre del pokemon en batalla
+         * la tercera es un array de objetos que hacemos igual a el array de pokemons del jugador 1
+         */
+        String type = "";
+        String namePokemon = getNamePokemon1PLayer2();
+        ArrayList<Object> pokemons = MethosInterfaceSelectionPokemon.getPokemonsPLayer2();
+
+        /**
+         * recorremos con un for el array de pokemons
+         */
+        for (int i = 0; i < pokemons.size(); i++) {
+
+            PokemonProperties pokemon = (PokemonProperties) pokemons.get(i);
+
+            /**
+             * si algun nombre coincide con el pokemon en batalla, entra en la condicion
+             */
+            if (pokemon.getName().equalsIgnoreCase(namePokemon)) {
+                /**
+                 * lo indicamos
+                 */
+                pokemon = (PokemonProperties) pokemons.get(i);
+
+                /**
+                 * asignamos la variable tipo al tipo del pokemon
+                 */
+                type = pokemon.getType();
+                break;
+
+
+            }
+        }
+        return type;
     }
 
 }
