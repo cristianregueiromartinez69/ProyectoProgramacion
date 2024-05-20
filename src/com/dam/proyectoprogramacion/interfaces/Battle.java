ef70497f8b66f346df5042641877a40c68c3ffe0
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-20 21:06:23 +0200

    vida actualizada y bajando correctamente, no me lo creo, revivio la redonda, revivio el esferico, revivio la caprichosa

--- src/com/dam/proyectoprogramacion/interfaces/Battle.java
+++ src/com/dam/proyectoprogramacion/interfaces/Battle.java
@@ -625,6 +625,8 @@ public class Battle extends JFrame {
         int currentLife = MethodsBattlePokemon.getLifePokemonsPlayer2().get(MethodsTextAreaBattlePlayer2.getNamePokemon1PLayer2());
         MethodsBattlePokemon.putValuesLifePokemonsPlayer2(MethodsTextAreaBattlePlayer2.getNamePokemon1PLayer2(), currentLife - damagePlayer1);
         LifePokemonInBattlePlayer2.getLifePokemonBarPlayer2().setValue(MethodsBattlePokemon.getLifePokemonsPlayer2().get(MethodsTextAreaBattlePlayer2.getNamePokemon1PLayer2()));
+        LifePokemonInBattlePlayer2.getLifePokemonBarPlayer2().setForeground(LifePokemonInBattlePlayer2.getBackgroundColorLifeBarPokemonPlayer2(MethodsBattlePokemon.getLifePokemonsPlayer2().get(MethodsTextAreaBattlePlayer2.getNamePokemon1PLayer2())));
+        LifePokemonInBattlePlayer2.getLifePokemonBarPlayer2().repaint();
 
         /**
          * cambiamos los turnos de los jugadores
