ea1b5b918a1c69b0217904a53b47e67d3bf6543d
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-20 10:42:25 +0200

    haciendo metodo para bajar la vida del pokemon rival y establecer turnos

--- src/com/dam/proyectoprogramacion/methods/battle/MethodsBattlePokemon.java
+++ src/com/dam/proyectoprogramacion/methods/battle/MethodsBattlePokemon.java
@@ -18,8 +18,8 @@ public class MethodsBattlePokemon {
      * atributos finales que serán la bonificacion de stab
      * la bonificaicon pir stab es si el ataque que usa el pokemon es el mismo que su propio tipo
      */
-   public static final float STAB = 1.5f;
-   public static final float NOT_STAB = 1.0f;
+    public static final float STAB = 1.5f;
+    public static final float NOT_STAB = 1.0f;
 
     /**
      * atributos finales que servirán para saber el daño que recibe el pokemon adersario
@@ -50,15 +50,6 @@ public class MethodsBattlePokemon {
      */
 
 
-
-
-
-
-
-
-
-
-
     //--------------------------------------------------------------------------//
 
 //----------------------------Jugador 1-----------------------------------//
@@ -67,10 +58,11 @@ public class MethodsBattlePokemon {
 
     /**
      * metodo para devolver la vida del pokemon en batalla del jugador 1
+     *
      * @param namePokemon el nombre del pokemon en batalla
      * @return la vida del pokemon
      */
-    public static int setValueOfLifePokemonInBattlePlayer1(String namePokemon){
+    public static int setValueOfLifePokemonInBattlePlayer1(String namePokemon) {
         /**
          * iniciamos la cantidad de defensa a 0
          */
@@ -117,17 +109,18 @@ public class MethodsBattlePokemon {
 
     /**
      * metodo para hacer la formula de daño del ataque 1 pokemon en batalla del jugador 1
-     * @param typePokemonUser el tipo del pokemon en batalla del jugador 1
-     * @param typeAttackPokemonUser el tipo del ataque 1 del pokemon en batalla del jugador 1
-     * @param powerAttackPokemonUser el poder de ataque del pokemon en batalla del jugador 1
+     *
+     * @param typePokemonUser          el tipo del pokemon en batalla del jugador 1
+     * @param typeAttackPokemonUser    el tipo del ataque 1 del pokemon en batalla del jugador 1
+     * @param powerAttackPokemonUser   el poder de ataque del pokemon en batalla del jugador 1
      * @param powerOfAttackPokemonUser la cantidad de ataque del pokemon en batalla del jugador 1
-     * @param typeOpposingPokemon el tipo de pokemon rival en batalla
-     * @param defenseOpppsingPokemon la defensa del pokemon rival en batalla
+     * @param typeOpposingPokemon      el tipo de pokemon rival en batalla
+     * @param defenseOpppsingPokemon   la defensa del pokemon rival en batalla
      * @return el daño que le hacemos
      */
     public static float getDamageAttacksPokemonInBattlePlayer1(String typePokemonUser, String typeAttackPokemonUser,
-                                                    int powerAttackPokemonUser, int powerOfAttackPokemonUser,
-                                                    String typeOpposingPokemon, int defenseOpppsingPokemon){
+                                                               int powerAttackPokemonUser, int powerOfAttackPokemonUser,
+                                                               String typeOpposingPokemon, int defenseOpppsingPokemon) {
         /**
          * asignamos una variable local de tipo float el metodo que nos dice si hay o no stab
          */
@@ -156,18 +149,18 @@ public class MethodsBattlePokemon {
     }
 
 
-
     /**
      * metodo para saber si el ataque 1 del pokemon en batalla pega por stab dle jugador 1
+     *
      * @param type el tipo del ataque 1 del pokemon en batalla
      * @return el daño por stab
      */
-    public static float getBonificationStabAttacksPokemonInBattlePlayer1(String type){
+    public static float getBonificationStabAttacksPokemonInBattlePlayer1(String type) {
 
         /**
          * recogemos en un array los tipos del pokemon desglosado
          */
-        String [] desgloseType = breakDownPokemonTypeInBattlePlayer1(MethodsTextAreaBattlePlayer1.getTypePokemonInBattlePlayer1());
+        String[] desgloseType = breakDownPokemonTypeInBattlePlayer1(MethodsTextAreaBattlePlayer1.getTypePokemonInBattlePlayer1());
         /**
          * variable local que inicialmente será de no stab
          */
@@ -175,11 +168,11 @@ public class MethodsBattlePokemon {
         /**
          * for para recorrer el tipo desglosado
          */
-        for(String auxType:desgloseType){
+        for (String auxType : desgloseType) {
             /**
              * si hay coincidencias de tipos, se le asigna el stab
              */
-            if(auxType.equalsIgnoreCase(type)){
+            if (auxType.equalsIgnoreCase(type)) {
                 auxDamage = STAB;
                 return auxDamage;
             }
@@ -188,16 +181,17 @@ public class MethodsBattlePokemon {
         /**
          * si no devolverá que no pega por stab
          */
-     return auxDamage;
+        return auxDamage;
     }
 
     /**
      * metodo para saber la efectividad del ataque al pokemon rival
-     * @param typeAttack el tipo del ataque que usa el pokemon
+     *
+     * @param typeAttack          el tipo del ataque que usa el pokemon
      * @param typeOpposingPokemon el tipo del rival
      * @return
      */
-    public static float getEffectivenessForTheOpposingPokemonPlayer1(String typeAttack, String typeOpposingPokemon){
+    public static float getEffectivenessForTheOpposingPokemonPlayer1(String typeAttack, String typeOpposingPokemon) {
 
         /**
          * iniciamos un string que almacenará el resultado en frase de la efectividad del ataque
@@ -213,22 +207,17 @@ public class MethodsBattlePokemon {
          * super eficaz
          * devolverá un valor o otro
          */
-        if(result.equalsIgnoreCase(MethodsTextAreaBattlePlayer1.NULL)){
+        if (result.equalsIgnoreCase(MethodsTextAreaBattlePlayer1.NULL)) {
             return NULL_DAMAGE;
-        }
-        else if(result.equalsIgnoreCase(MethodsTextAreaBattlePlayer1.VERY_INEFFECTIVE)){
+        } else if (result.equalsIgnoreCase(MethodsTextAreaBattlePlayer1.VERY_INEFFECTIVE)) {
             return VERY_INEFFECTIVE_DAMAGE;
-        }
-        else if(result.equalsIgnoreCase(MethodsTextAreaBattlePlayer1.INEFFECTIVE)){
+        } else if (result.equalsIgnoreCase(MethodsTextAreaBattlePlayer1.INEFFECTIVE)) {
             return INEFFECTIVE_DAMAGE;
-        }
-        else if(result.equalsIgnoreCase(MethodsTextAreaBattlePlayer1.NEUTRAL)){
+        } else if (result.equalsIgnoreCase(MethodsTextAreaBattlePlayer1.NEUTRAL)) {
             return NEUTRAL_DAMAGE;
-        }
-        else if(result.equalsIgnoreCase(MethodsTextAreaBattlePlayer1.EFFECTIVE)){
+        } else if (result.equalsIgnoreCase(MethodsTextAreaBattlePlayer1.EFFECTIVE)) {
             return EFFECTIVE_DAMAGE;
-        }
-        else{
+        } else {
             return SUPER_EFFECTIVE_DAMAGE;
         }
 
@@ -236,6 +225,7 @@ public class MethodsBattlePokemon {
 
     /**
      * metodo para desglosar el tipo del pokemon en batalla para verificar el stab
+     *
      * @param type el tipo del pokemon en batalla
      * @return el array con los 2 tipos del pokemon en caso de que tenga 2
      */
@@ -258,12 +248,14 @@ public class MethodsBattlePokemon {
 //----------------------------Jugador 2-----------------------------------//
 
 //--------------------------------------------------------------------------//
+
     /**
      * metodo para devolver la vida del pokemon en batalla del jugador 1
+     *
      * @param namePokemon el nombre del pokemon en batalla
      * @return la vida del pokemon
      */
-    public static int setValueOfLifePokemonInBattlePlayer2(String namePokemon){
+    public static int setValueOfLifePokemonInBattlePlayer2(String namePokemon) {
         /**
          * iniciamos la cantidad de defensa a 0
          */
@@ -308,20 +300,20 @@ public class MethodsBattlePokemon {
     }
 
 
-
     /**
      * metodo para hacer la formula de daño del ataque  pokemon en batalla del jugador 2
-     * @param typePokemonUser el tipo del pokemon en batalla del jugador 2
-     * @param typeAttackPokemonUser el tipo del ataque del pokemon en batalla del jugador 2
-     * @param powerAttackPokemonUser el poder de ataque del pokemon en batalla del jugador 2
+     *
+     * @param typePokemonUser          el tipo del pokemon en batalla del jugador 2
+     * @param typeAttackPokemonUser    el tipo del ataque del pokemon en batalla del jugador 2
+     * @param powerAttackPokemonUser   el poder de ataque del pokemon en batalla del jugador 2
      * @param powerOfAttackPokemonUser la cantidad de ataque del pokemon en batalla del jugador 2
-     * @param typeOpposingPokemon el tipo de pokemon rival en batalla
-     * @param defenseOpppsingPokemon la defensa del pokemon rival en batalla
+     * @param typeOpposingPokemon      el tipo de pokemon rival en batalla
+     * @param defenseOpppsingPokemon   la defensa del pokemon rival en batalla
      * @return el daño que le hacemos
      */
     public static float getDamageAttacksPokemonInBattlePlayer2(String typePokemonUser, String typeAttackPokemonUser,
                                                                int powerAttackPokemonUser, int powerOfAttackPokemonUser,
-                                                               String typeOpposingPokemon, int defenseOpppsingPokemon){
+                                                               String typeOpposingPokemon, int defenseOpppsingPokemon) {
         /**
          * asignamos una variable local de tipo float el metodo que nos dice si hay o no stab
          */
@@ -350,18 +342,18 @@ public class MethodsBattlePokemon {
     }
 
 
-
     /**
      * metodo para saber si el ataque 1 del pokemon en batalla pega por stab del jugador 2
+     *
      * @param type el tipo del ataque 1 del pokemon en batalla
      * @return el daño por stab
      */
-    public static float getBonificationStabAttacksPokemonInBattlePlayer2(String type){
+    public static float getBonificationStabAttacksPokemonInBattlePlayer2(String type) {
 
         /**
          * recogemos en un array los tipos del pokemon desglosado
          */
-        String [] desgloseType = breakDownPokemonTypeInBattlePlayer2(MethodsTextAreaBattlePlayer2.getTypePokemonInBattlePlayer2());
+        String[] desgloseType = breakDownPokemonTypeInBattlePlayer2(MethodsTextAreaBattlePlayer2.getTypePokemonInBattlePlayer2());
         /**
          * variable local que inicialmente será de no stab
          */
@@ -369,11 +361,11 @@ public class MethodsBattlePokemon {
         /**
          * for para recorrer el tipo desglosado
          */
-        for(String auxType:desgloseType){
+        for (String auxType : desgloseType) {
             /**
              * si hay coincidencias de tipos, se le asigna el stab
              */
-            if(auxType.equalsIgnoreCase(type)){
+            if (auxType.equalsIgnoreCase(type)) {
                 auxDamage = STAB;
                 return auxDamage;
             }
@@ -387,11 +379,12 @@ public class MethodsBattlePokemon {
 
     /**
      * metodo para saber la efectividad del ataque al pokemon rival
-     * @param typeAttack el tipo del ataque que usa el pokemon
+     *
+     * @param typeAttack          el tipo del ataque que usa el pokemon
      * @param typeOpposingPokemon el tipo del rival
      * @return
      */
-    public static float getEffectivenessForTheOpposingPokemonPlayer2(String typeAttack, String typeOpposingPokemon){
+    public static float getEffectivenessForTheOpposingPokemonPlayer2(String typeAttack, String typeOpposingPokemon) {
 
         /**
          * iniciamos un string que almacenará el resultado en frase de la efectividad del ataque
@@ -407,22 +400,17 @@ public class MethodsBattlePokemon {
          * super eficaz
          * devolverá un valor o otro
          */
-        if(result.equalsIgnoreCase(MethodsTextAreaBattlePlayer1.NULL)){
+        if (result.equalsIgnoreCase(MethodsTextAreaBattlePlayer1.NULL)) {
             return NULL_DAMAGE;
-        }
-        else if(result.equalsIgnoreCase(MethodsTextAreaBattlePlayer1.VERY_INEFFECTIVE)){
+        } else if (result.equalsIgnoreCase(MethodsTextAreaBattlePlayer1.VERY_INEFFECTIVE)) {
             return VERY_INEFFECTIVE_DAMAGE;
-        }
-        else if(result.equalsIgnoreCase(MethodsTextAreaBattlePlayer1.INEFFECTIVE)){
+        } else if (result.equalsIgnoreCase(MethodsTextAreaBattlePlayer1.INEFFECTIVE)) {
             return INEFFECTIVE_DAMAGE;
-        }
-        else if(result.equalsIgnoreCase(MethodsTextAreaBattlePlayer1.NEUTRAL)){
+        } else if (result.equalsIgnoreCase(MethodsTextAreaBattlePlayer1.NEUTRAL)) {
             return NEUTRAL_DAMAGE;
-        }
-        else if(result.equalsIgnoreCase(MethodsTextAreaBattlePlayer1.EFFECTIVE)){
+        } else if (result.equalsIgnoreCase(MethodsTextAreaBattlePlayer1.EFFECTIVE)) {
             return EFFECTIVE_DAMAGE;
-        }
-        else{
+        } else {
             return SUPER_EFFECTIVE_DAMAGE;
         }
 
@@ -430,6 +418,7 @@ public class MethodsBattlePokemon {
 
     /**
      * metodo para desglosar el tipo del pokemon en batalla para verificar el stab
+     *
      * @param type el tipo del pokemon en batalla
      * @return el array con los 2 tipos del pokemon en caso de que tenga 2
      */
@@ -456,12 +445,21 @@ public class MethodsBattlePokemon {
 
     /**
      * metodo para generar un numero aleatorio entre 85 y 100
+     *
      * @return el numero generado
      */
     public static float getValueOfVariation() {
 
-        int randomValue = 85 + (int)(Math.random() * 16);
+        int randomValue = 85 + (int) (Math.random() * 16);
         return (float) randomValue;
+    }
+
+    public static int damageOppossingPokemon(int speedPokemonPlayer1, int speedPokemonPlayer2){
+        int damage = 0;
+        if(speedPokemonPlayer1 > speedPokemonPlayer2){
+
+        }
+
     }
 
 }
