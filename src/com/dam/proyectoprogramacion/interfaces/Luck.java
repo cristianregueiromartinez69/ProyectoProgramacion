8244da5cbae06fde4b2fe8b17c00b80929894dc3
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-22 10:15:53 +0200

    quitando el parche de casa

--- src/com/dam/proyectoprogramacion/interfaces/Luck.java
+++ src/com/dam/proyectoprogramacion/interfaces/Luck.java
@@ -572,9 +572,7 @@ public class Luck extends JFrame {
      * @param e el objeto de tipo actionevent
      */
     public void winnerFaceOrTail(ActionEvent e){
-        MethodsInterfaceLuck.setAliasWinnerLuck(InformationPanelPlayer2Luck.getAliasTextPlayer2().getText());
-        new SelectionPokemon();
-            /**
+             /**
          * si se cumple la condicion de este metodo, se pued epulsar el boton
          * el metodo consiste en saber si está editable el alias del jugador 1 y 2
          */
