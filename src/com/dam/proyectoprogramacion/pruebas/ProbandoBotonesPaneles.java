1692036d7ebea4f07e64e09deb9406745e2b62b3
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-23 20:45:27 +0200

    sonido de garra metal funcionando

--- src/com/dam/proyectoprogramacion/pruebas/ProbandoBotonesPaneles.java
+++ src/com/dam/proyectoprogramacion/pruebas/ProbandoBotonesPaneles.java
@@ -10,7 +10,7 @@ public class ProbandoBotonesPaneles extends JFrame {
     public static void musicprueba() {
         try {
             // Cargar el archivo de audio
-            File audioFile = new File("canciones/enviteacuaticosonido.wav");
+            File audioFile = new File("canciones/garrametalsonido.wav");
             if (!audioFile.exists()) {
                 System.err.println("El archivo de audio no existe: " + audioFile.getAbsolutePath());
                 return;
