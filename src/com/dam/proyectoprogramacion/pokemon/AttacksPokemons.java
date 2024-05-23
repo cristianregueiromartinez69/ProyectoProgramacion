b336eb5e91856ba9a37ad3d32feca7186ef4804a
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-23 19:56:44 +0200

    haciendo metodo para devolver un array con los nombres de los ataques para ponerle sonidos a los ataques de los pokemons

--- src/com/dam/proyectoprogramacion/pokemon/AttacksPokemons.java
+++ src/com/dam/proyectoprogramacion/pokemon/AttacksPokemons.java
@@ -14,6 +14,21 @@ public class AttacksPokemons {
         this.precision = precision;
     }
 
+    public static String [] allAttacks(){
+        return new String[]{"pulso dragon", "hiperrayo", "viento hielo", "rayo",
+        "envite acuatico", "garra metal", "acua jet", "salmuera",
+        "enfado", "terremoto", "pataleta", "garra dragon",
+        "bola sombra", "pulso umbrio", "bomba acida", "carga toxica",
+        "ultra puño", "a bocajarro", "envite igneo", "acrobata",
+        "ataque oseo", "a bocajarro", "esfera aural", "onda vacio",
+        "mordisco", "voltio cruel", "colmillo rayo", "psicocolmillo",
+        "puño sombra", "a bocajarro", "golpe bajo", "puño trueno",
+        "rayo hielo", "surf", "hidrobomba", "cola dragon",
+        "fuerza equina", "machada", "gigaimpacto", "golpe cuerpo",
+        "martillo colosal", "desarme", "martillo de hielo", "sorpresa",
+        "roca afilada", "avalancha", "triturar", "pataleta"};
+    }
+
     public String getType() {
         return type;
     }
