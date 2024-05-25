eba3fc14d496a95778f0d1d21e0c1281af956202
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-25 16:22:32 +0200

    sonido de golpe cuerpo funcionando

--- src/com/dam/proyectoprogramacion/pruebas/ProbandoBotonesPaneles.java
+++ src/com/dam/proyectoprogramacion/pruebas/ProbandoBotonesPaneles.java
@@ -10,7 +10,7 @@ public class ProbandoBotonesPaneles extends JFrame {
     public static void musicprueba() {
         try {
             // Cargar el archivo de audio
-            File audioFile = new File("canciones/gigaimpactosonido.wav");
+            File audioFile = new File("canciones/golpecuerposonido.wav");
             if (!audioFile.exists()) {
                 System.err.println("El archivo de audio no existe: " + audioFile.getAbsolutePath());
                 return;
