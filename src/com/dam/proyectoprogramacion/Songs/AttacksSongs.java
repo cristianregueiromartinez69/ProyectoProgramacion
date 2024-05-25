6be88220325fa10ad2b4ccc5be149900a4d5d76d
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-25 16:50:06 +0200

    reutilizando codigo para los metodos de reproducir sonido de ataques

--- src/com/dam/proyectoprogramacion/Songs/AttacksSongs.java
+++ src/com/dam/proyectoprogramacion/Songs/AttacksSongs.java
@@ -1,5 +1,85 @@
 package com.dam.proyectoprogramacion.Songs;
 
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
 public class AttacksSongs {
 
-}
+
+    /**
+     * metodo que reproduce la musica del ataque a bocajarro
+     */
+    public static void musicAbocajarro() {
+        try {
+            /**
+             * Cargar el archivo de audio
+             */
+            File audioFile = new File("canciones/abocajarrosonido.wav");
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+
+            /**
+             *  Obtener el formato de audio
+             */
+            AudioFormat format = audioStream.getFormat();
+            DataLine.Info info = new DataLine.Info(Clip.class, format);
+
+            /**
+             * / Abrir el clip
+             */
+            Clip clip = (Clip) AudioSystem.getLine(info);
+            clip.open(audioStream);
+
+            /**
+             * / Reproducir el audio
+             */
+            clip.start();
+
+
+
+            /**
+             * capturamos la excepcion en caso de que no se reproduzca la musica
+             */
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * metodo que reproduce la musica del ataque de acrobata
+     */
+    public static void musicAcrobata() {
+        try {
+            /**
+             * Cargar el archivo de audio
+             */
+            File audioFile = new File("canciones/acrobatasonido.wav");
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+
+            /**
+             *  Obtener el formato de audio
+             */
+            AudioFormat format = audioStream.getFormat();
+            DataLine.Info info = new DataLine.Info(Clip.class, format);
+
+            /**
+             * / Abrir el clip
+             */
+            Clip clip = (Clip) AudioSystem.getLine(info);
+            clip.open(audioStream);
+
+            /**
+             * / Reproducir el audio
+             */
+            clip.start();
+
+
+
+            /**
+             * capturamos la excepcion en caso de que no se reproduzca la musica
+             */
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+    }
