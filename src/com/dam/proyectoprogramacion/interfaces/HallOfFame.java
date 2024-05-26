c5885431f5ef5980dc78b188bf4142ced7f6a7b4
Chema <jcaobaqueiro@danielcastelao.org>
2024-05-26 10:35:15 +0200

    Interfaz basica Hall

--- src/com/dam/proyectoprogramacion/interfaces/HallOfFame.java
+++ src/com/dam/proyectoprogramacion/interfaces/HallOfFame.java
@@ -1,13 +1,16 @@
 package com.dam.proyectoprogramacion.interfaces;
 import com.dam.proyectoprogramacion.background.*;
 import com.dam.proyectoprogramacion.buttons.*;
+import com.dam.proyectoprogramacion.Songs.HallOfFameSong;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 
-public class HallOfFame extends JFrame{
+public class HallOfFame extends JFrame {
 
     ButtonInterfaceSkipHall buttonsHall = new ButtonInterfaceSkipHall();
     private static JPanel backgroundImage;
@@ -16,29 +19,79 @@ public class HallOfFame extends JFrame{
     private static JPanel Pokemon2;
     private static JPanel Pokemon3;
 
-    public void HallofFame () {
+    public void HallofFame() {
 
         /**
          * Llamar a la musica que va a sonar en el hall of fame
          */
 
+        HallOfFameSong.hallOfFameMusic();
+
+        setTitle("Hall of Fame");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+
+        /**
+         * Variable con la imagen de fondo
+         */
 
         Image image = new ImageIcon("imagenes/Registro_en_el_Hall_de_la_Fama_Pt.png").getImage();
         backgroundImage.setLayout(new BorderLayout());
 
-        JPanel background = new JPanel();
+        /**
+         * Jpanel igual a un metodo el cual va a devolver un jpanel
+         */
+        JPanel background = makePanel();
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(800,700,700,700);
+        gbc.insets = new Insets(800, 700, 700, 700);
 
-        backgroundImage.add(background,BorderLayout.CENTER);
+        /**
+         * añadir el panel recien creado
+         */
+        backgroundImage.add(background, BorderLayout.CENTER);
 
         add(backgroundImage);
         setVisible(true);
 
         gbc.gridy = 0;
         gbc.gridx = 0;
+
+
         JButton back = buttonsHall.makeBackButton();
-        back.setPreferredSize(new Dimension(200,50));
-        backgroundImage.add(back,gbc);
+        back.setPreferredSize(new Dimension(200, 50));
+        backgroundImage.add(buttonsPanel(back),gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+
+
+        back.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                back.setBackground(Color.BLUE.brighter());
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e){back.setBackground(Color.WHITE);}
+        });
+
+
+    }
+
+    private JPanel makePanel() {
+        JPanel contentPanel = new JPanel();
+        /**
+         * tenemos que poner que el panel sea falso en opaco para que no se sobreponga
+         */
+        contentPanel.setOpaque(false);
+        contentPanel.setLayout(new GridBagLayout());
+        return contentPanel;
+    }
+
+    private JPanel buttonsPanel(JButton battle) {
+        JPanel skip = new JPanel();
+        skip.setLayout(new GridBagLayout());
+        skip.add(battle);
+        return skip;
     }
 }
