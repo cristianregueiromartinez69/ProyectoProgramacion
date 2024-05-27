8a023dedda74d89bf73dc8abe262c1b03dceabcd
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-27 14:27:06 +0200

    algunos test de sonidos

--- test/TestSongs.java
+++ test/TestSongs.java
@@ -1,2 +1,73 @@
+import com.dam.proyectoprogramacion.Songs.AttacksSongs;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.mockito.plugins.InstantiatorProvider2;
+import org.mockito.plugins.InstantiatorProvider;
+
+
+import javax.sound.sampled.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
 public class TestSongs {
+    @Mock
+    private AudioInputStream mockAudioInputStream;
+
+    @Mock
+    private Clip mockClip;
+
+    @Before
+    public void setUp() throws Exception {
+        // Mock the AudioSystem static methods
+        PowerMockito.mockStatic(AudioSystem.class);
+
+        // Mock the AudioInputStream returned by AudioSystem.getAudioInputStream
+        when(AudioSystem.getAudioInputStream(any(File.class))).thenReturn(mockAudioInputStream);
+
+        // Mock the Clip returned by AudioSystem.getLine
+        when(AudioSystem.getLine(any(DataLine.Info.class))).thenReturn(mockClip);
+    }
+
+    @Test
+    public void testMusicCloseCombat() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        // Call the method
+        AttacksSongs.musicCloseCombat();
+
+        // Verify that AudioSystem.getAudioInputStream was called
+        PowerMockito.verifyStatic(AudioSystem.class);
+        AudioSystem.getAudioInputStream(any(File.class));
+
+        // Verify that AudioSystem.getLine was called
+        PowerMockito.verifyStatic(AudioSystem.class);
+        AudioSystem.getLine(any(DataLine.Info.class));
+
+        // Verify that the clip was opened and started
+        verify(mockClip).open(mockAudioInputStream);
+        verify(mockClip).start();
+    }
+
+    @Test
+    public void testMusicAcrobatics() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        // Call the method
+        AttacksSongs.musicAcrobatics();
+
+        // Verify that AudioSystem.getAudioInputStream was called
+        PowerMockito.verifyStatic(AudioSystem.class);
+        AudioSystem.getAudioInputStream(any(File.class));
+
+        // Verify that AudioSystem.getLine was called
+        PowerMockito.verifyStatic(AudioSystem.class);
+        AudioSystem.getLine(any(DataLine.Info.class));
+
+        // Verify that the clip was opened and started
+        verify(mockClip).open(mockAudioInputStream);
+        verify(mockClip).start();
+    }
 }
