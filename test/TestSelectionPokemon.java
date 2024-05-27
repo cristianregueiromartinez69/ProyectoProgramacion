b1936d17efc25069c1090a742b4f3ccbada53067
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-27 20:48:52 +0200

    haciendo mas test de creacion de objetos y ataques pokemon

--- test/TestSelectionPokemon.java
+++ test/TestSelectionPokemon.java
@@ -1,12 +1,17 @@
+import com.dam.proyectoprogramacion.methods.battle.MethodsBattlePokemon;
 import com.dam.proyectoprogramacion.pokemon.*;
 import com.dam.proyectoprogramacion.methods.selectionpokemon.*;
 import org.junit.Test;
 import org.junit.Assert;
 import org.junit.jupiter.api.DisplayName;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Optional;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestSelectionPokemon {
 
 
@@ -235,6 +240,7 @@ public class TestSelectionPokemon {
         assertEquals(42.0f, luxray.getWeight(), 0.0f);
 
     }
+
     @Test
     @DisplayName("crear ataques Luxray")
     public void testLuxrayAttacks() {
@@ -250,5 +256,220 @@ public class TestSelectionPokemon {
         AttacksPokemons[] luxrayAttacks = luxray.getAttacks();
 
         assertArrayEquals(expectedAttacks, luxrayAttacks);
+    }
+
+    @Test
+    @DisplayName("crear Objeto Marshadow")
+    public void testMakeMarshadowObject() {
+        Marshadow marshadow = MethosInterfaceSelectionPokemonCreatePokemons.makeMarshadowObject();
+        assertNotNull(marshadow);
+        assertEquals("Marshadow", marshadow.getName());
+        assertEquals("lucha-fantasma", marshadow.getType());
+        assertEquals(50, (int) marshadow.getLevel());
+        assertEquals(170, (int) marshadow.getLife());
+        assertNull(marshadow.getSex());
+        assertEquals(108, (int) marshadow.getAttack());
+        assertEquals(85, (int) marshadow.getDefense());
+        assertEquals(194, (int) marshadow.getSpeed());
+        assertEquals(0.7f, marshadow.getSize(), 0.0f);
+        assertEquals(22.2f, marshadow.getWeight(), 0.0f);
+
+    }
+
+    @Test
+    @DisplayName("crear ataques Marshadow")
+    public void testMarshadowAttacks() {
+        AttacksPokemons[] expectedAttacks = {
+                new AttacksPokemons("Fantasma", "Roba sombra", 90, 100),
+                new AttacksPokemons("Hielo", "Puño hielo", 75, 100),
+                new AttacksPokemons("Lucha", "A bocajarro", 120, 100),
+                new AttacksPokemons("Fantasma", "Sombre vil", 40, 100)
+        };
+        Marshadow marshadow = MethosInterfaceSelectionPokemonCreatePokemons.makeMarshadowObject();
+        marshadow.setAttacks(expectedAttacks);
+
+        AttacksPokemons[] marshadowAttacks = marshadow.getAttacks();
+
+        assertArrayEquals(expectedAttacks, marshadowAttacks);
+    }
+
+    @Test
+    @DisplayName("crear Objeto Milotic")
+    public void testMakeMiloticObject() {
+        Milotic milotic = MethosInterfaceSelectionPokemonCreatePokemons.makeMiloticObject();
+        assertNotNull(milotic);
+        assertEquals("Milotic", milotic.getName());
+        assertEquals("agua", milotic.getType());
+        assertEquals(50, (int) milotic.getLevel());
+        assertEquals(202, (int) milotic.getLife());
+        assertEquals("Hembra", milotic.getSex());
+        assertEquals(80, (int) milotic.getAttack());
+        assertEquals(102, (int) milotic.getDefense());
+        assertEquals(138, (int) milotic.getSpeed());
+        assertEquals(6.2f, milotic.getSize(), 0.0f);
+        assertEquals(162.0f, milotic.getWeight(), 0.0f);
+    }
+
+    @Test
+    @DisplayName("crear ataques Milotic")
+    public void testMiloticAttacks() {
+        AttacksPokemons[] expectedAttacks = {
+                new AttacksPokemons("Agua", "Escaldar", 80, 100),
+                new AttacksPokemons("Hielo", "Niebla", 0, 100),
+                new AttacksPokemons("Veneno", "Toxico", 0, 100),
+                new AttacksPokemons("Normal", "Recuperacion", 0, 100)
+        };
+        Milotic milotic = MethosInterfaceSelectionPokemonCreatePokemons.makeMiloticObject();
+        milotic.setAttacks(expectedAttacks);
+
+        AttacksPokemons[] miloticAttacks = milotic.getAttacks();
+
+        assertArrayEquals(expectedAttacks, miloticAttacks);
+    }
+
+    @Test
+    @DisplayName("crear Objeto Snorlax")
+    public void testMakeSnorlaxObject() {
+        Snorlax snorlax = MethosInterfaceSelectionPokemonCreatePokemons.makeSnorlaxObject();
+        assertNotNull(snorlax);
+        assertEquals("Snorlax", snorlax.getName());
+        assertEquals("normal", snorlax.getType());
+        assertEquals(50, (int) snorlax.getLevel());
+        assertEquals(267, (int) snorlax.getLife());
+        assertEquals("Macho", snorlax.getSex());
+        assertEquals(88, (int) snorlax.getAttack());
+        assertEquals(88, (int) snorlax.getDefense());
+        assertEquals(31, (int) snorlax.getSpeed());
+        assertEquals(2.1f, snorlax.getSize(), 0.0f);
+        assertEquals(460.0f, snorlax.getWeight(), 0.0f);
+    }
+
+    @Test
+    @DisplayName("crear ataques Snorlax")
+    public void testSnorlaxAttacks() {
+        AttacksPokemons[] expectedAttacks = {
+                new AttacksPokemons("Normal", "Tambor", 0, 100),
+                new AttacksPokemons("Normal", "Golpe Cuerpo", 85, 100),
+                new AttacksPokemons("Psiquico", "Descanso", 0, 100),
+                new AttacksPokemons("Psiquico", "Cabezazo zen", 80, 90)
+        };
+        Snorlax snorlax = MethosInterfaceSelectionPokemonCreatePokemons.makeSnorlaxObject();
+        snorlax.setAttacks(expectedAttacks);
+
+        AttacksPokemons[] snorlaxAttacks = snorlax.getAttacks();
+
+        assertArrayEquals(expectedAttacks, snorlaxAttacks);
+    }
+
+    @Test
+    @DisplayName("crear Objeto Tinkaton")
+    public void testMakeTinkatonObject() {
+        Tinkaton tinkaton = MethosInterfaceSelectionPokemonCreatePokemons.makeTinkatonObject();
+        assertNotNull(tinkaton);
+        assertEquals("Tinkaton", tinkaton.getName());
+        assertEquals("hada-acero", tinkaton.getType());
+        assertEquals(50, (int) tinkaton.getLevel());
+        assertEquals(180, (int) tinkaton.getLife());
+        assertEquals("Hembra", tinkaton.getSex());
+        assertEquals(73, (int) tinkaton.getAttack());
+        assertEquals(91, (int) tinkaton.getDefense());
+        assertEquals(160, (int) tinkaton.getSpeed());
+        assertEquals(0.7f, tinkaton.getSize(), 0.0f);
+        assertEquals(112.8f, tinkaton.getWeight(), 0.0f);
+    }
+
+    @Test
+    @DisplayName("crear ataques Tinkaton")
+    public void testTinkatonAttacks() {
+        AttacksPokemons[] expectedAttacks = {
+                new AttacksPokemons("Acero", "Martillo colosal", 160, 100),
+                new AttacksPokemons("Siniestro", "Desarme", 65, 100),
+                new AttacksPokemons("Hielo", "Martillo de hielo", 100, 90),
+                new AttacksPokemons("Normal", "Sorpresa", 40, 100)
+        };
+        Tinkaton tinkaton = MethosInterfaceSelectionPokemonCreatePokemons.makeTinkatonObject();
+        tinkaton.setAttacks(expectedAttacks);
+
+        AttacksPokemons[] tinkatonAttacks = tinkaton.getAttacks();
+
+        assertArrayEquals(expectedAttacks, tinkatonAttacks);
+    }
+
+    @Test
+    @DisplayName("crear Objeto Tyranitar")
+    public void testMakeTyranitarObject() {
+        Tyranitar tyranitar = MethosInterfaceSelectionPokemonCreatePokemons.makeTyranitarObject();
+        assertNotNull(tyranitar);
+        assertEquals("Tyranitar", tyranitar.getName());
+        assertEquals("roca-siniestro", tyranitar.getType());
+        assertEquals(50, (int) tyranitar.getLevel());
+        assertEquals(207, (int) tyranitar.getLife());
+        assertEquals("Hembra", tyranitar.getSex());
+        assertEquals(115, (int) tyranitar.getAttack());
+        assertEquals(105, (int) tyranitar.getDefense());
+        assertEquals(110, (int) tyranitar.getSpeed());
+        assertEquals(2.0f, tyranitar.getSize(), 0.0f);
+        assertEquals(202.0f, tyranitar.getWeight(), 0.0f);
+    }
+
+    @Test
+    @DisplayName("crear ataques Tyranitar")
+    public void testTyranitarAttacks() {
+        AttacksPokemons[] expectedAttacks = {
+                new AttacksPokemons("Roca", "Roca afilada", 100, 80),
+                new AttacksPokemons("Siniestro", "Desarme", 65, 100),
+                new AttacksPokemons("Electrico", "Onda trueno", 0, 90),
+                new AttacksPokemons("Tierra", "Terremoto", 100, 100)
+        };
+        Tyranitar tyranitar = MethosInterfaceSelectionPokemonCreatePokemons.makeTyranitarObject();
+        tyranitar.setAttacks(expectedAttacks);
+
+        AttacksPokemons[] tyranitarAttacks = tyranitar.getAttacks();
+
+        assertArrayEquals(expectedAttacks, tyranitarAttacks);
+    }
+
+
+    @Test
+    @DisplayName("comprobar que se añaden jugadores y pokemons correctamente")
+    public void testPutPlayersAndPokemons() {
+        String alias = "Player1";
+        ArrayList<Object> pokemons = new ArrayList<>();
+        pokemons.add("Pokemon1");
+        pokemons.add("Pokemon2");
+
+        MethosInterfaceSelectionPokemon.putPlayersAndPokemons(alias, pokemons);
+
+        HashMap<String, ArrayList<Object>> playersBattle = MethosInterfaceSelectionPokemon.getPlayersBattle();
+
+        assertTrue(playersBattle.containsKey(alias));
+        assertEquals(pokemons, playersBattle.get(alias));
+    }
+
+    @Test
+    @DisplayName("comprobar que se añade vida a los pokemons correctamente")
+    public void testAddLifePokemonsPlayer1() {
+        // Crear un alias y una lista de pokemons
+        String alias = "Player1";
+        ArrayList<Object> pokemons = new ArrayList<>();
+        Drampa pokemon1 = new Drampa();
+        pokemon1.setName("Pokemon1");
+        pokemon1.setLife(100);
+        pokemons.add(pokemon1);
+
+        Empoleon pokemon2 = new Empoleon();
+        pokemon2.setName("Pokemon2");
+        pokemon2.setLife(200);
+        pokemons.add(pokemon2);
+
+        MethosInterfaceSelectionPokemon.putPlayersAndPokemons(alias, pokemons);
+
+        MethosInterfaceSelectionPokemon.addLifePokemonsPlayer1(alias);
+
+        HashMap<String, Integer> lifePokemonsPlayer1 = MethodsBattlePokemon.getLifePokemonsPlayer1();
+
+        // Comprobar que los pokemons y sus vidas se añadieron correctamente
+        assertEquals(100, lifePokemonsPlayer1.get("pokemon1"));
+        assertEquals(200, lifePokemonsPlayer1.get("pokemon2"));
     }
 }
