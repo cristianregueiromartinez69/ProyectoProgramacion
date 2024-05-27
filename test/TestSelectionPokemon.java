07de7b0618d777c694a61a6b7703dcf8b4ceab5a
cristianregueiromartinez69 <cregueiromartinez@danielcastelao.org>
2024-05-27 14:42:22 +0200

    test de creacion de pokemons

--- test/TestSelectionPokemon.java
+++ test/TestSelectionPokemon.java
@@ -1,2 +1,29 @@
+import com.dam.proyectoprogramacion.pokemon.Drampa;
+import com.dam.proyectoprogramacion.methods.selectionpokemon.*;
+import com.dam.proyectoprogramacion.pokemon.Empoleon;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
 public class TestSelectionPokemon {
+
+
+    @Test
+    public void testMakeDrampaObject() {
+        Drampa drampa = MethosInterfaceSelectionPokemonCreatePokemons.makeDrampaObject();
+        assertNotNull(drampa);
+        assertEquals("Drampa", drampa.getName());
+        assertEquals("normal-dragon", drampa.getType());
+
+    }
+
+    @Test
+    public void testMakeEmpoleonObject() {
+        Empoleon empoleon = MethosInterfaceSelectionPokemonCreatePokemons.makeEmpoleonObject();
+        assertNotNull(empoleon);
+        assertEquals("Empoleon", empoleon.getName());
+        assertEquals("agua-acero", empoleon.getType());
+    }
+
 }
