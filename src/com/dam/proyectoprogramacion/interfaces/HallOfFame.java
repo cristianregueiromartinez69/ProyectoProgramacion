fbfd466b62a0e177f781c2b5891acda12a7033da
Chema <jcaobaqueiro@danielcastelao.org>
2024-06-02 10:49:53 +0200

    Ya funcionan los paneles de pokemon en el hall

--- src/com/dam/proyectoprogramacion/interfaces/HallOfFame.java
+++ src/com/dam/proyectoprogramacion/interfaces/HallOfFame.java
@@ -16,10 +16,6 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
-
-
-
-
 public class HallOfFame extends JFrame {
 
     private static JPanel pokemon1;
@@ -29,75 +25,60 @@ public class HallOfFame extends JFrame {
     private static ImageIcon pokemon1Player1_img = new ImageIcon(putImagePokemon1InBattlePlayer1());
     private static ImageIcon pokemon2Player1_img = new ImageIcon(putImagePokemon2InBattlePlayer1());
     private static ImageIcon pokemon3Player1_img = new ImageIcon(putImagePokemon3InBattlePlayer1());
-    private static final JLabel labelPokemon1Player1 = new JLabel("pokemon1", pokemon1Player1_img,JLabel.CENTER);
-    private static final JLabel labelPokemon2Player1 = new JLabel("pokemon2", pokemon2Player1_img,JLabel.CENTER);
-    private static final JLabel labelPokemon3Player1 = new JLabel("pokemon3", pokemon3Player1_img,JLabel.CENTER);
+    private static final JLabel labelPokemon1Player1 = new JLabel("", pokemon1Player1_img,JLabel.CENTER);
+    private static final JLabel labelPokemon2Player1 = new JLabel("", pokemon2Player1_img,JLabel.CENTER);
+    private static final JLabel labelPokemon3Player1 = new JLabel("", pokemon3Player1_img,JLabel.CENTER);
     private static ImageIcon pokemon1Player2_img = new ImageIcon(putImagePokemonInBattle1Player2());
     private static ImageIcon pokemon2Player2_img = new ImageIcon(putImagePokemonInBattle2Player2());
     private static ImageIcon pokemon3Player2_img = new ImageIcon(putImagePokemonInBattle3Player2());
-    private static final JLabel labelPokemon1Player2 = new JLabel("pokemon1", pokemon1Player2_img,JLabel.CENTER);
-    private static final JLabel labelPokemon2Player2 = new JLabel("pokemon2", pokemon2Player2_img,JLabel.CENTER);
-    private static final JLabel labelPokemon3Player2 = new JLabel("pokemon3", pokemon3Player2_img,JLabel.CENTER);
+    private static final JLabel labelPokemon1Player2 = new JLabel("", pokemon1Player2_img,JLabel.CENTER);
+    private static final JLabel labelPokemon2Player2 = new JLabel("", pokemon2Player2_img,JLabel.CENTER);
+    private static final JLabel labelPokemon3Player2 = new JLabel("", pokemon3Player2_img,JLabel.CENTER);
 
     MethosInterfaceSelectionPokemon seleccion = new MethosInterfaceSelectionPokemon();
     MethodsBattlePokemon equipo = new MethodsBattlePokemon();
     ButtonInterfaceSkipHall buttonsHall = new ButtonInterfaceSkipHall();
 
-
     public HallOfFame() {
-
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
-
-        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
         setBounds(600,600,600,600);
 
-        /**
-         * Llamar a la musica que va a sonar en el hall of fame
-         */
-
+        // Llamar a la musica que va a sonar en el hall of fame
         HallOfFameSong.hallOfFameMusic();
 
-
-        /**
-         * Variable con la imagen de fondo
-         */
-
+        // Variable con la imagen de fondo
         Image image = new ImageIcon("imagenes/Hall.png").getImage();
         BackgroundHallOfFame backgroundHall = new BackgroundHallOfFame(image);
-        backgroundHall.setLayout(new BorderLayout());
+        backgroundHall.setLayout(new GridBagLayout());
 
-        /**
-         * Jpanel igual a un metodo el cual va a devolver un jpanel
-         */
+        // Crear panel de fondo
         JPanel background = makePanel();
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(800, 700, 700, 700);
-
-        /**
-         * añadir el panel recien creado
-         */
-        backgroundHall.add(background, BorderLayout.CENTER);
+        gbc.insets = new Insets(20, 20, 20, 20);  // Aumentar espacio entre componentes
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.anchor = GridBagConstraints.CENTER;
+        backgroundHall.add(background, gbc);
 
         add(backgroundHall);
         setVisible(true);
 
-        gbc.gridy = 1;
-        gbc.gridx = 1;
-
-
+        // Configurar botones
+        gbc.gridwidth = 1;
+        gbc.gridy++;
         JButton back = buttonsHall.makeBackButton();
         back.setPreferredSize(new Dimension(300, 100));
-        background.add(buttonBackPanel(back), gbc);
+        background.add(back, gbc);
 
-        gbc.gridx = (int) 0.8;
-        gbc.gridy = (int) 0.8;
-
+        gbc.gridx++;
         JButton playAgain = buttonsHall.makePlayAgainButton();
         playAgain.setPreferredSize(new Dimension(300, 100));
-        background.add(playAgainPannel(playAgain), gbc);
+        background.add(playAgain, gbc);
 
-
+        // Agregar eventos de ratón a los botones
         back.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
@@ -122,6 +103,7 @@ public class HallOfFame extends JFrame {
             }
         });
 
+        // Agregar eventos de acción a los botones
         back.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -140,129 +122,68 @@ public class HallOfFame extends JFrame {
             }
         });
 
-        JFrame frame = new JFrame("Pokémon Game");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridBagLayout());
-
-        pokemon1 = new JPanel();
-        pokemon1 = pokemon1Panel();
-        pokemon1.setPreferredSize(new Dimension(100, 100));
-
-
-        pokemon2 = new JPanel();
-        pokemon2 = pokemon2Panel();
-        pokemon2.setPreferredSize(new Dimension(300, 300));
-
-        pokemon3 = new JPanel();
-        pokemon3 = pokemon3Panel();
-        pokemon3.setPreferredSize(new Dimension(300, 300));
-
-
-        player = new JPanel();
-        player = playerPanel();
-        player.setPreferredSize(new Dimension(200, 200));
-
+        // Configurar y agregar paneles de Pokémon
         gbc.gridx = 0;
-        gbc.gridy = 1;
-        frame.add(player, gbc);
+        gbc.gridy++;
+        pokemon1 = pokemon1Panel();
+        background.add(pokemon1, gbc);
 
-        GridBagConstraints fr = new GridBagConstraints();
-        fr.gridx = 0;
-        fr.gridy = 0;
-        fr.insets = new Insets(10, 10, 10, 10); // Espacio entre los paneles
-        frame.add(pokemon1, fr);
-        fr.gridx = 1;
-        frame.add(pokemon2, fr);
+        gbc.gridx++;
+        pokemon2 = pokemon2Panel();
+        background.add(pokemon2, gbc);
 
-        fr.gridx = 2;
-        frame.add(pokemon3, fr);
+        gbc.gridx++;
+        pokemon3 = pokemon3Panel();
+        background.add(pokemon3, gbc);
 
-        frame.pack();
-        frame.setVisible(true);
+        gbc.gridx++;
+        player = playerPanel();
+        player.add(new JLabel("Player Info"));
+        background.add(player, gbc);
 
-        background.add(frame);
-
-
-        if (equipo.isTeamPokemonAlivePlayer1()==true){
+        if (equipo.isTeamPokemonAlivePlayer1()) {
             pokemon1.add(labelPokemon1Player1);
             pokemon2.add(labelPokemon2Player1);
             pokemon3.add(labelPokemon3Player1);
-            background.add(Battle.globalPanelPLayer1);
-        }else{
+        } else {
             pokemon1.add(labelPokemon1Player2);
             pokemon2.add(labelPokemon2Player2);
             pokemon3.add(labelPokemon3Player2);
-            background.add(Battle.globalPanelPLayer2);
         }
     }
 
-
     private JPanel makePanel() {
         JPanel contentPanel = new JPanel();
-        /**
-         * tenemos que poner que el panel sea falso en opaco para que no se sobreponga
-         */
         contentPanel.setOpaque(false);
         contentPanel.setLayout(new GridBagLayout());
         return contentPanel;
     }
 
-    private JPanel buttonBackPanel(JButton back) {
-        JPanel skip = new JPanel();
-        skip.setLayout(new GridBagLayout());
-        skip.add(back);
-        return skip;
-    }
-
-    private JPanel playAgainPannel(JButton play) {
-        JPanel playAgainPannel = new JPanel();
-        playAgainPannel.setLayout(new GridBagLayout());
-        playAgainPannel.add(play);
-        return playAgainPannel;
-    }
-
-
-
-    /**
-     * metodos para realizar los paneles de los pokemon y devolverlos
-     * @return pokemon1
-     */
-    public static JPanel pokemon1Panel(){
+    public static JPanel pokemon1Panel() {
         JPanel pokemon1 = new JPanel();
-        pokemon1.setLayout(new BoxLayout(pokemon1,BoxLayout.X_AXIS));
+        pokemon1.setLayout(new BoxLayout(pokemon1, BoxLayout.Y_AXIS));
+        pokemon1.setOpaque(false);
         return pokemon1;
     }
 
-    /**
-     * metodo para realizar los paneles de los pokemon y devolverlos
-     * @return pokemon2
-     */
-
-    public static JPanel pokemon2Panel(){
+    public static JPanel pokemon2Panel() {
         JPanel pokemon2 = new JPanel();
-        pokemon2.setLayout(new BoxLayout(pokemon2,BoxLayout.X_AXIS));
+        pokemon2.setLayout(new BoxLayout(pokemon2, BoxLayout.Y_AXIS));
+        pokemon2.setOpaque(false);
         return pokemon2;
     }
 
-    /**
-     * metodo para realizar los paneles de los pokemon y devolverlos
-     * @return pokemon3
-     */
-
-    public static JPanel pokemon3Panel(){
+    public static JPanel pokemon3Panel() {
         JPanel pokemon3 = new JPanel();
-        pokemon3.setLayout(new BoxLayout(pokemon3,BoxLayout.X_AXIS));
+        pokemon3.setLayout(new BoxLayout(pokemon3, BoxLayout.Y_AXIS));
+        pokemon3.setOpaque(false);
         return pokemon3;
     }
 
-    /**
-     * metodo para realizar los paneles del jugador y devolverlos
-     * @return player
-     */
-
-    public static JPanel playerPanel(){
+    public static JPanel playerPanel() {
         JPanel player = new JPanel();
-        player.setLayout(new BoxLayout(player,BoxLayout.X_AXIS));
+        player.setLayout(new BoxLayout(player, BoxLayout.Y_AXIS));
+        player.setOpaque(false);
         return player;
     }
 
